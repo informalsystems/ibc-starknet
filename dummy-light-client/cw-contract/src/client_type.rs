d526f73e8cb94632bbaaf3d5f2a9545e581a6269
Ranadeep Biswas <mail@rnbguy.at>
2024-07-25 14:57:33 +0200

    rm dummy light client

--- dummy-light-client/cw-contract/src/client_type.rs
+++
@@ -1,9 +0,0 @@
-use ibc_client_cw::api::ClientType;
-use ibc_client_dummy::{ClientState, ConsensusState};
-
-pub struct DummyLightClient;
-
-impl<'a> ClientType<'a> for DummyLightClient {
-    type ClientState = ClientState;
-    type ConsensusState = ConsensusState;
-}
--- dummy-light-client/impls/src/lib.rs
+++
@@ -1,5 +0,0 @@
-mod client_state;
-mod consensus_state;
-
-pub use client_state::*;
-pub use consensus_state::*;
