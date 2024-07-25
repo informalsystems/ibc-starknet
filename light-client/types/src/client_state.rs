20282c4544fb55571d8b8e3cee3aabc4c5e674ae
Ranadeep Biswas <mail@rnbguy.at>
2024-07-25 14:59:10 +0200

    update starknet light client

--- light-client/types/src/client_state.rs
+++ light-client/types/src/client_state.rs
@@ -1,2 +1,40 @@
-#[derive(Clone, Debug, PartialEq)]
-pub struct ClientState {}
+use ibc_core::client::types::error::ClientError;
+use ibc_core::client::types::Height;
+use ibc_core::primitives::proto::{Any, Protobuf};
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Debug, PartialEq, derive_more::From)]
+pub struct ClientState {
+    pub latest_height: Height,
+}
+
+impl Protobuf<Any> for ClientState {}
+
+impl TryFrom<Any> for ClientState {
+    type Error = ClientError;
+
+    fn try_from(raw: Any) -> Result<Self, Self::Error> {
+        if raw.type_url != "/DummyClientState" {
+            return Err(ClientError::from("invalid type URL or empty value"));
+        }
+
+        let revision_number = u64::from_be_bytes(raw.value.try_into().unwrap());
+
+        Ok(Self {
+            latest_height: Height::min(revision_number),
+        })
+    }
+}
+
+impl From<ClientState> for Any {
+    fn from(client_state: ClientState) -> Self {
+        Self {
+            type_url: "/DummyClientState".to_string(),
+            value: client_state
+                .latest_height
+                .revision_number()
+                .to_be_bytes()
+                .to_vec(),
+        }
+    }
+}
--- light-client/types/src/consensus_state.rs
+++ light-client/types/src/consensus_state.rs
@@ -1,2 +1,48 @@
-#[derive(Clone, Debug, PartialEq)]
-pub struct ConsensusState {}
+use ibc_core::client::context::consensus_state::ConsensusState as ConsensusStateTrait;
+use ibc_core::client::types::error::ClientError;
+use ibc_core::commitment_types::commitment::CommitmentRoot;
+use ibc_core::primitives::proto::{Any, Protobuf};
+use ibc_core::primitives::Timestamp;
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Clone, Debug, PartialEq, derive_more::From)]
+pub struct ConsensusState {
+    root: CommitmentRoot,
+}
+
+impl ConsensusState {
+    pub fn root(&self) -> &CommitmentRoot {
+        &self.root
+    }
+}
+
+impl Default for ConsensusState {
+    fn default() -> Self {
+        Self {
+            root: CommitmentRoot::from(vec![]),
+        }
+    }
+}
+
+impl Protobuf<Any> for ConsensusState {}
+
+impl TryFrom<Any> for ConsensusState {
+    type Error = ClientError;
+
+    fn try_from(raw: Any) -> Result<Self, Self::Error> {
+        if raw.type_url != "/DummyConsensusState" || !raw.value.is_empty() {
+            return Err(ClientError::from("invalid type URL or empty value"));
+        }
+
+        Ok(Self::default())
+    }
+}
+
+impl From<ConsensusState> for Any {
+    fn from(consensus_state: ConsensusState) -> Self {
+        Self {
+            type_url: "/DummyConsensusState".to_string(),
+            value: vec![],
+        }
+    }
+}
