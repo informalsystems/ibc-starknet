773b409cbeefaa2bc8673e049132bdb02dfe131d
Soares Chen <soares.chen@maybevoid.com>
2024-08-08 14:08:43 +0200

    Implement EncodeArray

---
+++ relayer/crates/cairo-encoding-components/src/impls/encode/iter.rs
@@ -0,0 +1,21 @@
+use crate::traits::encode_mut::{CanEncodeMut, MutEncoder};
+
+pub struct EncodeArray;
+
+impl<Encoding, Strategy, Value> MutEncoder<Encoding, Strategy, Value> for EncodeArray
+where
+    Encoding: for<'a> CanEncodeMut<Strategy, <&'a Value as IntoIterator>::Item>,
+    for<'a> &'a Value: IntoIterator,
+{
+    fn encode_mut(
+        encoding: &Encoding,
+        value: &Value,
+        buffer: &mut Encoding::EncodeBuffer,
+    ) -> Result<(), Encoding::Error> {
+        for item in value.into_iter() {
+            encoding.encode_mut(&item, buffer)?;
+        }
+
+        Ok(())
+    }
+}
--- relayer/crates/cairo-encoding-components/src/impls/encode/mod.rs
+++ relayer/crates/cairo-encoding-components/src/impls/encode/mod.rs
@@ -1,3 +1,6 @@
+pub mod array;
 pub mod felt;
 pub mod from_felt;
+pub mod iter;
 pub mod u256;
+pub mod u64;
