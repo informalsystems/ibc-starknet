5792cf696bf7882d6f315de47a7e9883753e46bf
Soares Chen <soares.chen@maybevoid.com>
2024-08-22 14:06:46 +0200

    Implement protobuf encoding for StarknetClientState

--- light-client/ibc-client-starknet-types/src/client_state.rs
+++ light-client/ibc-client-starknet-types/src/client_state.rs
@@ -1,6 +1,8 @@
 use ibc_core::client::types::error::ClientError;
 use ibc_core::client::types::Height;
 use ibc_core::primitives::proto::{Any, Protobuf};
+use ibc_proto::ibc::core::client::v1::Height as ProtoHeight;
+use prost::Message;
 
 pub const CLIENT_STATE_TYPE_URL: &str = "/StarknetClientState";
 
@@ -10,23 +12,54 @@ pub struct ClientState {
     pub latest_height: Height,
 }
 
+#[derive(Message)]
+pub struct ProtoClientState {
+    #[prost(message, tag = "1")]
+    pub latest_height: Option<ProtoHeight>,
+}
+
 impl Protobuf<Any> for ClientState {}
+
+impl TryFrom<ProtoClientState> for ClientState {
+    type Error = ClientError;
+
+    fn try_from(proto_client_state: ProtoClientState) -> Result<Self, Self::Error> {
+        let latest_height = proto_client_state
+            .latest_height
+            .ok_or(ClientError::InvalidHeight)?
+            .try_into()
+            .map_err(|_| ClientError::InvalidHeight)?;
+
+        let client_state = ClientState { latest_height };
+
+        Ok(client_state)
+    }
+}
+
+impl From<ClientState> for ProtoClientState {
+    fn from(client_state: ClientState) -> Self {
+        ProtoClientState {
+            latest_height: Some(client_state.latest_height.into()),
+        }
+    }
+}
 
 impl TryFrom<Any> for ClientState {
     type Error = ClientError;
 
-    fn try_from(raw: Any) -> Result<Self, Self::Error> {
+    fn try_from(raw: Any) -> Result<Self, ClientError> {
         if raw.type_url != CLIENT_STATE_TYPE_URL {
             return Err(ClientError::UnknownClientStateType {
                 client_state_type: raw.type_url,
             });
         }
 
-        let revision_number = u64::from_be_bytes(raw.value.try_into().unwrap());
+        let proto_client_state =
+            ProtoClientState::decode(raw.value.as_ref()).map_err(|e| ClientError::Other {
+                description: e.to_string(),
+            })?;
 
-        Ok(Self {
-            latest_height: Height::min(revision_number),
-        })
+        proto_client_state.try_into()
     }
 }
 
@@ -34,11 +67,7 @@ impl From<ClientState> for Any {
     fn from(client_state: ClientState) -> Self {
         Self {
             type_url: CLIENT_STATE_TYPE_URL.to_string(),
-            value: client_state
-                .latest_height
-                .revision_number()
-                .to_be_bytes()
-                .to_vec(),
+            value: ProtoClientState::from(client_state).encode_to_vec(),
         }
     }
 }
