eded154cd126bd71eeb928618b07315de6dfd732
Soares Chen <soares.chen@maybevoid.com>
2024-08-22 14:17:40 +0200

    Implement protobuf encoding for StarknetConsensusState

--- light-client/ibc-client-starknet-types/src/consensus_state.rs
+++ light-client/ibc-client-starknet-types/src/consensus_state.rs
@@ -1,6 +1,7 @@
 use ibc_core::client::types::error::ClientError;
 use ibc_core::commitment_types::commitment::CommitmentRoot;
 use ibc_core::primitives::proto::{Any, Protobuf};
+use prost::Message;
 
 pub const CONSENSUS_STATE_TYPE_URL: &str = "/StarknetConsensusState";
 
@@ -8,6 +9,12 @@ pub const CONSENSUS_STATE_TYPE_URL: &str = "/StarknetConsensusState";
 #[derive(Clone, Debug, PartialEq, derive_more::From)]
 pub struct ConsensusState {
     root: CommitmentRoot,
+}
+
+#[derive(Message)]
+pub struct ProtoConsensusState {
+    #[prost(message, tag = "1")]
+    pub root: Option<Vec<u8>>,
 }
 
 impl ConsensusState {
@@ -36,7 +43,12 @@ impl TryFrom<Any> for ConsensusState {
             });
         }
 
-        Ok(Self::default())
+        let proto_consensus_state =
+            ProtoConsensusState::decode(raw.value.as_ref()).map_err(|e| ClientError::Other {
+                description: e.to_string(),
+            })?;
+
+        proto_consensus_state.try_into()
     }
 }
 
@@ -44,7 +56,30 @@ impl From<ConsensusState> for Any {
     fn from(consensus_state: ConsensusState) -> Self {
         Self {
             type_url: CONSENSUS_STATE_TYPE_URL.to_string(),
-            value: vec![],
+            value: ProtoConsensusState::from(consensus_state).encode_to_vec(),
         }
+    }
+}
+
+impl From<ConsensusState> for ProtoConsensusState {
+    fn from(consensus_state: ConsensusState) -> Self {
+        ProtoConsensusState {
+            root: Some(consensus_state.root.into_vec()),
+        }
+    }
+}
+
+impl TryFrom<ProtoConsensusState> for ConsensusState {
+    type Error = ClientError;
+
+    fn try_from(proto_consensus_state: ProtoConsensusState) -> Result<Self, Self::Error> {
+        let root = proto_consensus_state
+            .root
+            .ok_or_else(|| ClientError::Other {
+                description: "empty commitment root".into(),
+            })?
+            .into();
+
+        Ok(ConsensusState { root })
     }
 }
