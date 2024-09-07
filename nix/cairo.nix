3380564e8f3e9714a4c9250fa1241d8ac55a1454
Soares Chen <soares.chen@maybevoid.com>
2024-09-07 08:09:01 +0200

    Remove unneeded changes

--- nix/cairo.nix
+++ nix/cairo.nix
@@ -11,18 +11,13 @@ let
     cargoHash = "sha256-piabK7UNyt2hWoqR5IDnCZoo8+VvSthSao3sQXKjE0o=";
 
     OPENSSL_NO_VENDOR = 1;
-    AARCH64_APPLE_DARWIN_OPENSSL_NO_VENDOR = 1;
     PKG_CONFIG_PATH = "${nixpkgs.openssl.dev}/lib/pkgconfig";
 
     nativeBuildInputs = [
-      nixpkgs.libiconv
       nixpkgs.pkg-config
     ];
 
     buildInputs = nixpkgs.lib.optionals nixpkgs.stdenv.isDarwin [
-      nixpkgs.libiconv
-      nixpkgs.pkg-config
-      nixpkgs.darwin.apple_sdk.frameworks.Security
       nixpkgs.darwin.apple_sdk.frameworks.SystemConfiguration
     ];
   };
