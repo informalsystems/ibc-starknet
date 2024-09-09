cb5ec17511711da4ccd56d24c5462156938c10bf
Soares Chen <soares.chen@maybevoid.com>
2024-09-09 18:59:25 +0000

    Implement StarknetClientHeader for StarknetClientHeader

--- relayer/crates/starknet-chain-components/src/types/client_header.rs
+++ relayer/crates/starknet-chain-components/src/types/client_header.rs
@@ -1,4 +1,5 @@
 use hermes_encoding_components::traits::convert::{CanConvert, Converter};
+use hermes_encoding_components::traits::decode::CanDecode;
 use hermes_encoding_components::traits::encode::CanEncode;
 use hermes_encoding_components::traits::types::encoded::HasEncodedType;
 use hermes_protobuf_encoding_components::types::ViaAny;
@@ -27,5 +28,23 @@ where
         };
 
         encoding.convert(&wasm_message)
+    }
+}
+
+impl<Encoding> Converter<Encoding, Any, StarknetClientHeader> for ConvertStarknetClientHeader
+where
+    Encoding: HasEncodedType<Encoded = Vec<u8>>
+        + CanDecode<ViaAny, StarknetConsensusState>
+        + CanConvert<Any, ClientMessage>,
+{
+    fn convert(
+        encoding: &Encoding,
+        header_any: &Any,
+    ) -> Result<StarknetClientHeader, Encoding::Error> {
+        let client_message = encoding.convert(header_any)?;
+
+        let consensus_state = encoding.decode(&client_message.data)?;
+
+        Ok(StarknetClientHeader { consensus_state })
     }
 }
--- relayer/crates/starknet-chain-context/src/contexts/encoding/protobuf.rs
+++ relayer/crates/starknet-chain-context/src/contexts/encoding/protobuf.rs
@@ -1,10 +1,11 @@
 use cgp::core::error::{DelegateErrorRaiser, ErrorRaiserComponent, ErrorTypeComponent};
 use cgp::prelude::*;
-use hermes_encoding_components::traits::convert::CanConvert;
+use hermes_encoding_components::traits::convert::CanConvertBothWays;
 use hermes_encoding_components::traits::encode_and_decode::CanEncodeAndDecode;
 use hermes_error::impls::ProvideHermesError;
 use hermes_protobuf_encoding_components::types::{Any, ViaProtobuf};
 use hermes_starknet_chain_components::components::encoding::protobuf::*;
+use hermes_starknet_chain_components::types::client_header::StarknetClientHeader;
 use hermes_starknet_chain_components::types::client_state::{
     StarknetClientState, WasmStarknetClientState,
 };
@@ -42,14 +43,11 @@ pub trait CanUseStarknetProtobufEncoding:
     CanEncodeAndDecode<ViaProtobuf, StarknetClientState>
     + CanEncodeAndDecode<ViaProtobuf, StarknetConsensusState>
     + CanEncodeAndDecode<ViaProtobuf, ClientMessage>
-    + CanConvert<StarknetClientState, Any>
-    + CanConvert<Any, StarknetClientState>
-    + CanConvert<StarknetConsensusState, Any>
-    + CanConvert<Any, StarknetConsensusState>
-    + CanConvert<WasmStarknetClientState, Any>
-    + CanConvert<Any, WasmStarknetClientState>
-    + CanConvert<WasmStarknetConsensusState, Any>
-    + CanConvert<Any, WasmStarknetConsensusState>
+    + CanConvertBothWays<StarknetClientState, Any>
+    + CanConvertBothWays<StarknetConsensusState, Any>
+    + CanConvertBothWays<WasmStarknetClientState, Any>
+    + CanConvertBothWays<StarknetClientHeader, Any>
+    + CanConvertBothWays<WasmStarknetConsensusState, Any>
 {
 }
 
