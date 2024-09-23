0d8669723a2dbaed25a1343e4268942aa93e8a70
Soares Chen <soares.chen@gmail.com>
2024-09-23 15:52:16 +0200

    Test performing dummy IBC handshake with Wasm Starknet light client on Cosmos (#89)
    * Use get_block_with_tx_hashes to query chain status
    
    * Init Cosmos connection ID
    
    * Ad hoc connection ack is working
    
    * Channel initialization is working
    
    * Channel open ack is working
    
    * Add comments

--- relayer/crates/starknet-integration-tests/tests/light_client.rs
+++ relayer/crates/starknet-integration-tests/tests/light_client.rs
@@ -5,16 +5,27 @@ use std::path::PathBuf;
 use std::sync::Arc;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
+use hermes_cosmos_chain_components::traits::message::ToCosmosMessage;
+use hermes_cosmos_chain_components::types::messages::channel::open_ack::CosmosChannelOpenAckMessage;
+use hermes_cosmos_chain_components::types::messages::channel::open_init::CosmosChannelOpenInitMessage;
+use hermes_cosmos_chain_components::types::messages::connection::open_ack::CosmosConnectionOpenAckMessage;
+use hermes_cosmos_chain_components::types::messages::connection::open_init::CosmosConnectionOpenInitMessage;
 use hermes_cosmos_integration_tests::init::init_test_runtime;
 use hermes_cosmos_relayer::contexts::build::CosmosBuilder;
 use hermes_cosmos_relayer::contexts::chain::CosmosChain;
+use hermes_cosmos_relayer::contexts::encoding::CosmosEncoding;
 use hermes_cosmos_wasm_relayer::context::cosmos_bootstrap::CosmosWithWasmClientBootstrap;
+use hermes_encoding_components::traits::convert::CanConvert;
 use hermes_error::types::Error;
+use hermes_relayer_components::chain::traits::payload_builders::create_client::CanBuildCreateClientPayload;
 use hermes_relayer_components::chain::traits::queries::chain_status::{
     CanQueryChainHeight, CanQueryChainStatus,
 };
 use hermes_relayer_components::chain::traits::queries::client_state::CanQueryClientState;
 use hermes_relayer_components::chain::traits::queries::consensus_state::CanQueryConsensusState;
+use hermes_relayer_components::chain::traits::send_message::CanSendSingleMessage;
+use hermes_relayer_components::chain::traits::types::ibc_events::channel::HasChannelOpenInitEvent;
+use hermes_relayer_components::chain::traits::types::ibc_events::connection::HasConnectionOpenInitEvent;
 use hermes_relayer_components::relay::traits::client_creator::CanCreateClient;
 use hermes_relayer_components::relay::traits::target::DestinationTarget;
 use hermes_relayer_components::relay::traits::update_client_message_builder::CanSendTargetUpdateClientMessage;
@@ -25,6 +36,13 @@ use hermes_starknet_integration_tests::contexts::bootstrap::StarknetBootstrap;
 use hermes_starknet_relayer::contexts::starknet_to_cosmos_relay::StarknetToCosmosRelay;
 use hermes_test_components::bootstrap::traits::chain::CanBootstrapChain;
 use hermes_test_components::chain_driver::traits::types::chain::HasChain;
+use ibc::core::channel::types::channel::State;
+use ibc::core::connection::types::version::Version;
+use ibc_proto::ibc::core::channel::v1::{Channel, Counterparty};
+use ibc_relayer::chain::cosmos::client::Settings;
+use ibc_relayer::config::types::TrustThreshold;
+use ibc_relayer_types::core::ics04_channel::channel::Ordering;
+use ibc_relayer_types::Height;
 use sha2::{Digest, Sha256};
 
 #[test]
@@ -155,6 +173,108 @@ fn test_starknet_light_client() -> Result<(), Error> {
                 consensus_state.consensus_state.root.into_vec(),
                 starknet_status.block_hash.to_bytes_be()
             );
+        }
+
+        let connection_id = {
+            let open_init_message = CosmosConnectionOpenInitMessage {
+                client_id: client_id.clone(),
+                counterparty_client_id: client_id.clone(), // TODO: stub
+                counterparty_commitment_prefix: "ibc".into(),
+                version: Version::compatibles().pop().unwrap(),
+                delay_period: Duration::from_secs(0),
+            };
+
+            let events = cosmos_chain.send_message(open_init_message.to_cosmos_message()).await?;
+
+            let connection_id = events
+                .into_iter()
+                .find_map(<CosmosChain as HasConnectionOpenInitEvent<StarknetChain>>::try_extract_connection_open_init_event)
+                .unwrap()
+                .connection_id
+            ;
+
+            println!("initialized connection on Cosmos: {connection_id}");
+
+            connection_id
+        };
+
+        {
+            // Pretend that we have relayed ConnectionOpenTry to Starknet, and then send ConnectionOpenAck.
+
+            let create_client_settings = Settings {
+                max_clock_drift: Duration::from_secs(40),
+                trusting_period: None,
+                trust_threshold: TrustThreshold::ONE_THIRD,
+            };
+
+            let payload = <CosmosChain as CanBuildCreateClientPayload<CosmosChain>>::build_create_client_payload(cosmos_chain, &create_client_settings,
+            ).await?;
+
+            let client_state = CosmosEncoding.convert(&payload.client_state)?;
+
+            runtime.sleep(Duration::from_secs(1)).await;
+
+            let open_ack_message = CosmosConnectionOpenAckMessage {
+                connection_id: connection_id.clone(),
+                counterparty_connection_id: connection_id.clone(), // TODO: stub
+                version: Version::compatibles().pop().unwrap(),
+                client_state,
+                update_height: Height::new(0, 1).unwrap(),
+                proof_try: [0; 32].into(), // dummy proofs
+                proof_client: [0; 32].into(),
+                proof_consensus: [0; 32].into(),
+                proof_consensus_height: payload.client_state.latest_height,
+            };
+
+            cosmos_chain.send_message(open_ack_message.to_cosmos_message()).await?;
+        }
+
+        let channel_id = {
+            let channel = Channel {
+                state: State::Init as i32,
+                ordering: Ordering::Unordered as i32,
+                counterparty: Some(Counterparty {
+                    port_id: "11b7f9bfa43d3facae74efa5dfe0030df98273271278291d67c16a4e6cd5f7c".to_string(), // stub application contract on Starknet as port ID
+                    channel_id: "".to_string(),
+                }),
+                connection_hops: vec![connection_id.to_string()],
+                version: "ics20-1".into(),
+                upgrade_sequence: 0,
+            };
+
+            let open_init_message = CosmosChannelOpenInitMessage {
+                port_id: "transfer".into(),
+                channel,
+            };
+
+            let events = cosmos_chain.send_message(open_init_message.to_cosmos_message()).await?;
+
+
+            let channel_id = events
+                .into_iter()
+                .find_map(<CosmosChain as HasChannelOpenInitEvent<StarknetChain>>::try_extract_channel_open_init_event)
+                .unwrap()
+                .channel_id
+            ;
+
+            println!("initialized channel on Cosmos: {channel_id}");
+
+            channel_id
+        };
+
+        {
+            // Pretend that we have already done ChannelOpenTry on Starknet, and then continue with ChannelOpenAck
+
+            let open_ack_message = CosmosChannelOpenAckMessage {
+                port_id: "transfer".into(),
+                channel_id: channel_id.to_string(),
+                counterparty_channel_id: "63c350000c404581a3385ec7b4324008b2965dd8fc5af768b87329d25e57cfa".into(), // stub channel contract on Starknet as channel ID
+                counterparty_version: "ics20-1".into(),
+                update_height: Height::new(0, 1).unwrap(),
+                proof_try: [0; 32].into(), // dummy proofs
+            };
+
+            cosmos_chain.send_message(open_ack_message.to_cosmos_message()).await?;
         }
 
         Ok(())
