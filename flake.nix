489c92b2d86e425e177d7c793b84d08eeeb046ae
Soares Chen <soares.chen@maybevoid.com>
2024-12-09 12:55:56 +0000

    Include PKG_CONFIG_PATH

--- flake.nix
+++ flake.nix
@@ -102,7 +102,7 @@
               taplo
               just
               openssl
-              pkgconfig
+              pkg-config
               ;
 
             nixfmt = nixpkgs.nixfmt-rfc-style;
@@ -130,7 +130,10 @@
           devShells = {
             default = nixpkgs.mkShell { buildInputs = shell-deps; };
 
-            rust = nixpkgs.mkShell { buildInputs = [ rust ] ++ shell-deps; };
+            rust = nixpkgs.mkShell {
+              PKG_CONFIG_PATH = "${nixpkgs.openssl.dev}/lib/pkgconfig";
+              buildInputs = [ rust ] ++ shell-deps;
+            };
 
             rust-nightly = nixpkgs.mkShell { buildInputs = [ rust-nightly ] ++ shell-deps; };
 
