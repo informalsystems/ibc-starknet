57d7aa1c7d58c1a5c56cc929624f63d2e40348b1
Rano | Ranadeep <mail@rnbguy.at>
2024-12-16 20:05:53 +0100

    feat(relayer): `ConnectionEndWithProofsQuerier` and `ConnectionEndQuerier` (#139)
    * impl ConnectionEnd type
    
    * encoder for ConnectionEnd
    
    * use starknet ConnectionEnd
    
    * impl ConnectionEndQuerier
    
    * update trait brounds
    
    * impl ConnectionEndWithProofsQuerier
    
    * happy clippy

--- relayer/crates/starknet-chain-components/src/impls/queries/connection_end.rs
+++ relayer/crates/starknet-chain-components/src/impls/queries/connection_end.rs
@@ -1,4 +1,8 @@
-use cgp::prelude::HasErrorType;
+use core::marker::PhantomData;
+
+use cgp::prelude::*;
+use hermes_cairo_encoding_components::strategy::ViaCairo;
+use hermes_cairo_encoding_components::types::as_felt::AsFelt;
 use hermes_chain_components::traits::queries::connection_end::{
     ConnectionEndQuerier, ConnectionEndWithProofsQuerier,
 };
@@ -6,40 +10,102 @@ use hermes_chain_components::traits::types::connection::HasConnectionEndType;
 use hermes_chain_components::traits::types::height::HasHeightType;
 use hermes_chain_components::traits::types::ibc::HasConnectionIdType;
 use hermes_chain_components::traits::types::proof::HasCommitmentProofType;
+use hermes_encoding_components::traits::decode::CanDecode;
+use hermes_encoding_components::traits::encode::CanEncode;
+use hermes_encoding_components::traits::has_encoding::HasEncoding;
+use hermes_encoding_components::traits::types::encoded::HasEncodedType;
+use starknet::core::types::Felt;
+use starknet::macros::selector;
+
+use crate::traits::contract::call::CanCallContract;
+use crate::traits::queries::address::CanQueryContractAddress;
+use crate::traits::types::blob::HasBlobType;
+use crate::traits::types::method::HasSelectorType;
+use crate::types::commitment_proof::StarknetCommitmentProof;
+use crate::types::connection_id::{ConnectionEnd, ConnectionId};
 
 pub struct QueryConnectionEndFromStarknet;
 
-impl<Chain, Counterparty> ConnectionEndQuerier<Chain, Counterparty>
+impl<Chain, Counterparty, Encoding> ConnectionEndQuerier<Chain, Counterparty>
     for QueryConnectionEndFromStarknet
 where
     Chain: HasHeightType
-        + HasConnectionIdType<Counterparty>
-        + HasConnectionEndType<Counterparty>
-        + HasErrorType,
+        + HasConnectionIdType<Counterparty, ConnectionId = ConnectionId>
+        + HasConnectionEndType<Counterparty, ConnectionEnd = ConnectionEnd>
+        + HasBlobType<Blob = Vec<Felt>>
+        + HasSelectorType<Selector = Felt>
+        + CanQueryContractAddress<symbol!("ibc_core_contract_address")>
+        + HasEncoding<AsFelt, Encoding = Encoding>
+        + CanCallContract
+        + CanRaiseError<Encoding::Error>,
+    Encoding: CanEncode<ViaCairo, ConnectionId>
+        + CanDecode<ViaCairo, ConnectionEnd>
+        + HasEncodedType<Encoded = Vec<Felt>>,
 {
     async fn query_connection_end(
-        _chain: &Chain,
-        _connection_id: &Chain::ConnectionId,
+        chain: &Chain,
+        connection_id: &Chain::ConnectionId,
         _height: &Chain::Height,
     ) -> Result<Chain::ConnectionEnd, Chain::Error> {
-        todo!()
+        // TODO(rano): how to query at a specific height?
+
+        let encoding = chain.encoding();
+
+        let contract_address = chain.query_contract_address(PhantomData).await?;
+
+        let calldata = encoding.encode(connection_id).map_err(Chain::raise_error)?;
+
+        let output = chain
+            .call_contract(&contract_address, &selector!("connection_end"), &calldata)
+            .await?;
+
+        encoding.decode(&output).map_err(Chain::raise_error)
     }
 }
 
-impl<Chain, Counterparty> ConnectionEndWithProofsQuerier<Chain, Counterparty>
+impl<Chain, Counterparty, Encoding> ConnectionEndWithProofsQuerier<Chain, Counterparty>
     for QueryConnectionEndFromStarknet
 where
     Chain: HasHeightType
-        + HasCommitmentProofType
-        + HasConnectionIdType<Counterparty>
-        + HasConnectionEndType<Counterparty>
-        + HasErrorType,
+        + HasCommitmentProofType<CommitmentProof = StarknetCommitmentProof>
+        + HasConnectionIdType<Counterparty, ConnectionId = ConnectionId>
+        + HasConnectionEndType<Counterparty, ConnectionEnd = ConnectionEnd>
+        + HasBlobType<Blob = Vec<Felt>>
+        + HasSelectorType<Selector = Felt>
+        + CanQueryContractAddress<symbol!("ibc_core_contract_address")>
+        + HasEncoding<AsFelt, Encoding = Encoding>
+        + CanCallContract
+        + CanRaiseError<Encoding::Error>,
+    Encoding: CanEncode<ViaCairo, ConnectionId>
+        + CanDecode<ViaCairo, ConnectionEnd>
+        + HasEncodedType<Encoded = Vec<Felt>>,
 {
     async fn query_connection_end_with_proofs(
-        _chain: &Chain,
-        _connection_id: &Chain::ConnectionId,
+        chain: &Chain,
+        connection_id: &Chain::ConnectionId,
         _height: &Chain::Height,
     ) -> Result<(Chain::ConnectionEnd, Chain::CommitmentProof), Chain::Error> {
-        todo!()
+        // TODO(rano): how to query at a specific height?
+
+        let encoding = chain.encoding();
+
+        let contract_address = chain.query_contract_address(PhantomData).await?;
+
+        let calldata = encoding.encode(connection_id).map_err(Chain::raise_error)?;
+
+        let output = chain
+            .call_contract(&contract_address, &selector!("connection_end"), &calldata)
+            .await?;
+
+        // TODO(rano): how to get the proof?
+        let dummy_proof = StarknetCommitmentProof {
+            proof_height: 0,
+            proof_bytes: vec![],
+        };
+
+        Ok((
+            encoding.decode(&output).map_err(Chain::raise_error)?,
+            dummy_proof,
+        ))
     }
 }
--- relayer/crates/starknet-chain-components/src/types/connection_id.rs
+++ relayer/crates/starknet-chain-components/src/types/connection_id.rs
@@ -2,11 +2,15 @@ use core::fmt::{Display, Formatter};
 
 use cgp::core::component::UseContext;
 use cgp::prelude::*;
+use hermes_cairo_encoding_components::impls::encode_mut::variant_from::EncodeVariantFrom;
 use hermes_encoding_components::impls::encode_mut::combine::CombineEncoders;
 use hermes_encoding_components::impls::encode_mut::field::EncodeField;
 use hermes_encoding_components::impls::encode_mut::from::DecodeFrom;
-use hermes_encoding_components::traits::transform::Transformer;
+use hermes_encoding_components::traits::transform::{Transformer, TransformerRef};
 use hermes_wasm_encoding_components::components::{MutDecoderComponent, MutEncoderComponent};
+
+use crate::types::client_id::ClientId;
+use crate::types::messages::ibc::connection::{BasePrefix, ConnectionVersion};
 
 #[derive(Debug, PartialEq, Clone, HasField)]
 pub struct ConnectionId {
@@ -36,5 +40,134 @@ impl Transformer for EncodeConnectionId {
 impl Display for ConnectionId {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
         self.connection_id.fmt(f)
+    }
+}
+
+#[derive(HasField)]
+pub struct ConnectionEnd {
+    pub state: ConnectionState,
+    pub client_id: ClientId,
+    pub counterparty: ConnectionState,
+    pub version: ConnectionVersion,
+    pub delay_period: u64,
+}
+
+pub struct EncodeConnectionEnd;
+
+delegate_components! {
+    EncodeConnectionEnd {
+        MutEncoderComponent: CombineEncoders<Product![
+            EncodeField<symbol!("state"), UseContext>,
+            EncodeField<symbol!("client_id"), UseContext>,
+            EncodeField<symbol!("counterparty"), UseContext>,
+            EncodeField<symbol!("version"), UseContext>,
+            EncodeField<symbol!("delay_period"), UseContext>,
+        ]>,
+        MutDecoderComponent: DecodeFrom<Self, UseContext>,
+    }
+}
+
+impl Transformer for EncodeConnectionEnd {
+    type From = Product![
+        ConnectionState,
+        ClientId,
+        ConnectionState,
+        ConnectionVersion,
+        u64
+    ];
+    type To = ConnectionEnd;
+
+    fn transform(
+        product![state, client_id, counterparty, version, delay_period,]: Self::From,
+    ) -> ConnectionEnd {
+        ConnectionEnd {
+            state,
+            client_id,
+            counterparty,
+            version,
+            delay_period,
+        }
+    }
+}
+
+pub enum ConnectionState {
+    Uninitialized,
+    Init,
+    TryOpen,
+    Open,
+}
+
+pub struct EncodeConnectionState;
+
+delegate_components! {
+    EncodeConnectionState {
+        [
+            MutEncoderComponent,
+            MutDecoderComponent,
+        ]: EncodeVariantFrom<Self>,
+    }
+}
+
+impl TransformerRef for EncodeConnectionState {
+    type From = ConnectionState;
+    type To<'a> = Sum![(), (), (), ()];
+
+    fn transform<'a>(from: &'a ConnectionState) -> Sum![(), (), (), ()] {
+        match from {
+            ConnectionState::Uninitialized => Either::Left(()),
+            ConnectionState::Init => Either::Right(Either::Left(())),
+            ConnectionState::TryOpen => Either::Right(Either::Right(Either::Left(()))),
+            ConnectionState::Open => Either::Right(Either::Right(Either::Right(Either::Left(())))),
+        }
+    }
+}
+
+impl Transformer for EncodeConnectionState {
+    type From = Sum![(), (), (), ()];
+    type To = ConnectionState;
+
+    fn transform(value: Sum![(), (), (), ()]) -> ConnectionState {
+        match value {
+            Either::Left(()) => ConnectionState::Uninitialized,
+            Either::Right(Either::Left(())) => ConnectionState::Init,
+            Either::Right(Either::Right(Either::Left(()))) => ConnectionState::TryOpen,
+            Either::Right(Either::Right(Either::Right(Either::Left(())))) => ConnectionState::Open,
+            Either::Right(Either::Right(Either::Right(Either::Right(value)))) => match value {},
+        }
+    }
+}
+
+#[derive(HasField)]
+pub struct ConnectionCounterparty {
+    pub client_id: ClientId,
+    pub connection_id: ConnectionId,
+    pub prefix: BasePrefix,
+}
+
+pub struct EncodeConnectionCounterparty;
+
+delegate_components! {
+    EncodeConnectionCounterparty {
+        MutEncoderComponent: CombineEncoders<Product![
+            EncodeField<symbol!("client_id"), UseContext>,
+            EncodeField<symbol!("connection_id"), UseContext>,
+            EncodeField<symbol!("prefix"), UseContext>,
+        ]>,
+        MutDecoderComponent: DecodeFrom<Self, UseContext>,
+    }
+}
+
+impl Transformer for EncodeConnectionCounterparty {
+    type From = Product![ClientId, ConnectionId, BasePrefix];
+    type To = ConnectionCounterparty;
+
+    fn transform(
+        product![client_id, connection_id, prefix,]: Self::From,
+    ) -> ConnectionCounterparty {
+        ConnectionCounterparty {
+            client_id,
+            connection_id,
+            prefix,
+        }
     }
 }
