defe948685648fd19c70a216ae2b5785cc485264
Ranadeep Biswas <mail@rnbguy.at>
2024-12-16 19:31:08 +0100

    impl ConnectionEndWithProofsQuerier

--- relayer/crates/starknet-chain-components/src/impls/queries/connection_end.rs
+++ relayer/crates/starknet-chain-components/src/impls/queries/connection_end.rs
@@ -21,6 +21,7 @@ use crate::traits::contract::call::CanCallContract;
 use crate::traits::queries::address::CanQueryContractAddress;
 use crate::traits::types::blob::HasBlobType;
 use crate::traits::types::method::HasSelectorType;
+use crate::types::commitment_proof::StarknetCommitmentProof;
 use crate::types::connection_id::{ConnectionEnd, ConnectionId};
 
 pub struct QueryConnectionEndFromStarknet;
@@ -62,20 +63,49 @@ where
     }
 }
 
-impl<Chain, Counterparty> ConnectionEndWithProofsQuerier<Chain, Counterparty>
+impl<Chain, Counterparty, Encoding> ConnectionEndWithProofsQuerier<Chain, Counterparty>
     for QueryConnectionEndFromStarknet
 where
     Chain: HasHeightType
-        + HasCommitmentProofType
-        + HasConnectionIdType<Counterparty>
-        + HasConnectionEndType<Counterparty>
-        + HasErrorType,
+        + HasCommitmentProofType<CommitmentProof = StarknetCommitmentProof>
+        + HasConnectionIdType<Counterparty, ConnectionId = ConnectionId>
+        + HasConnectionEndType<Counterparty, ConnectionEnd = ConnectionEnd>
+        + HasBlobType<Blob = Vec<Felt>>
+        + HasSelectorType<Selector = Felt>
+        + CanQueryContractAddress<symbol!("ibc_core_contract_address")>
+        + HasEncoding<AsFelt, Encoding = Encoding>
+        + CanCallContract
+        + CanRaiseError<Encoding::Error>,
+    Encoding: CanEncode<ViaCairo, ConnectionId>
+        + CanDecode<ViaCairo, ConnectionEnd>
+        + HasEncodedType<Encoded = Vec<Felt>>,
 {
     async fn query_connection_end_with_proofs(
-        _chain: &Chain,
-        _connection_id: &Chain::ConnectionId,
+        chain: &Chain,
+        connection_id: &Chain::ConnectionId,
         _height: &Chain::Height,
     ) -> Result<(Chain::ConnectionEnd, Chain::CommitmentProof), Chain::Error> {
-        todo!()
+        // TODO(rano): how to query at a specific height?
+
+        let encoding = chain.encoding();
+
+        let contract_address = chain.query_contract_address(PhantomData).await?;
+
+        let calldata = encoding.encode(connection_id).map_err(Chain::raise_error)?;
+
+        let output = chain
+            .call_contract(&contract_address, &selector!("connection_end"), &calldata)
+            .await?;
+
+        // TODO(rano): how to get the proof?
+        let dummy_proof = StarknetCommitmentProof {
+            proof_height: 0,
+            proof_bytes: vec![],
+        };
+
+        Ok((
+            encoding.decode(&output).map_err(Chain::raise_error)?,
+            dummy_proof,
+        ))
     }
 }
