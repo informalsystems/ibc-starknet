7c543d1b498aafaaecd1393a73b6f198d148dfca
Luca Joss <luca@informal.systems>
2024-12-18 16:47:26 +0100

    clippy fix

--- relayer/crates/starknet-chain-components/src/types/client_id.rs
+++ relayer/crates/starknet-chain-components/src/types/client_id.rs
@@ -53,9 +53,9 @@ impl FromStr for ClientId {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         // TODO: Verify that the parsing from str is correct
         let values: Vec<&str> = s.split('-').collect();
-        let felt_str = values.get(0).ok_or_else(|| format!("client ID doesn't have correct format, expecting `<client_type>-<sequence>, got `{s}`"))?;
+        let felt_str = values.first().ok_or_else(|| format!("client ID doesn't have correct format, expecting `<client_type>-<sequence>, got `{s}`"))?;
         let sequence_str = values.get(1).ok_or_else(|| format!("client ID doesn't have correct format, expecting `<client_type>-<sequence>, got `{s}`"))?;
-        let client_type = Felt::from_str(&felt_str)
+        let client_type = Felt::from_str(felt_str)
             .map_err(|e| format!("failed to parse {felt_str} to Felt. Cause: {e}"))?;
         let sequence = sequence_str
             .parse::<u64>()
