fa58035f158d6931c9c081714caa1d76098d254a
Luca Joss <luca@informal.systems>
2024-12-18 17:06:08 +0100

    Use Starknet chain config from Starknet relayer config instead of creating a new one

--- relayer/crates/starknet-cli/src/impls/build.rs
+++ relayer/crates/starknet-cli/src/impls/build.rs
@@ -5,12 +5,8 @@ use hermes_cli_components::traits::types::config::HasConfigType;
 use hermes_cosmos_relayer::contexts::build::CosmosBuilder;
 use hermes_runtime::types::runtime::HermesRuntime;
 use hermes_runtime_components::traits::runtime::HasRuntime;
-use hermes_starknet_chain_components::impls::types::config::{
-    StarknetChainConfig, StarknetRelayerConfig,
-};
-use hermes_starknet_chain_components::types::wallet::StarknetWallet;
+use hermes_starknet_chain_components::impls::types::config::StarknetRelayerConfig;
 use hermes_starknet_relayer::contexts::builder::StarknetBuilder;
-use starknet::macros::felt;
 
 pub struct LoadStarknetBuilder;
 
@@ -28,7 +24,6 @@ where
         let cosmos_chain_config = config.cosmos_chain_config.ok_or_else(|| {
             App::raise_error("missing Cosmos chain config in Starknet relayer config")
         })?;
-        let rpc_addr = cosmos_chain_config.rpc_addr.to_string();
 
         let cosmos_builder = CosmosBuilder::new(
             vec![cosmos_chain_config],
@@ -39,19 +34,9 @@ where
             Default::default(),
         );
 
-        // Extracted from relayer/crates/starknet-test-components/src/impls/bootstrap/bootstrap_chain.rs
-        let relayer_wallet = StarknetWallet {
-            account_address: felt!(
-                "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691"
-            ),
-            signing_key: felt!("0x71d7bb07b9a64f6f78ac4c816aff4da9"),
-            public_key: felt!("0x39d9e6ce352ad4530a0ef5d5a18fd3303c3606a7fa6ac5b620020ad681cc33b"),
-        };
-
-        let starknet_chain_config = StarknetChainConfig {
-            json_rpc_url: rpc_addr,
-            relayer_wallet,
-        };
+        let starknet_chain_config = config.starknet_chain_config.ok_or_else(|| {
+            App::raise_error("missing Starknet chain config in Starknet relayer config")
+        })?;
 
         let builder = StarknetBuilder::new(cosmos_builder, runtime, starknet_chain_config);
 
