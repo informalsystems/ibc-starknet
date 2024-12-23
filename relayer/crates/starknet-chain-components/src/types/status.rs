6ba231b1d8d1523a389f399b9a21fd782ec54b11
Luca Joss <luca@informal.systems>
2024-12-23 11:29:03 +0100

    Add query chain-status CLI

--- relayer/crates/starknet-chain-components/src/types/status.rs
+++ relayer/crates/starknet-chain-components/src/types/status.rs
@@ -1,9 +1,22 @@
+use core::fmt::{Display, Formatter, Result};
+
 use hermes_cosmos_chain_components::types::status::Time;
+use serde::Serialize;
 use starknet::core::types::Felt;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 pub struct StarknetChainStatus {
     pub height: u64,
     pub block_hash: Felt,
     pub time: Time,
+}
+
+impl Display for StarknetChainStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(
+            f,
+            "height: {}, block_hash: {}, time: {}",
+            self.height, self.block_hash, self.time
+        )
+    }
 }
--- relayer/crates/starknet-cli/src/commands/query/subcommand.rs
+++ relayer/crates/starknet-cli/src/commands/query/subcommand.rs
@@ -1,3 +1,4 @@
+use hermes_cli_components::impls::commands::queries::chain_status::QueryChainStatusArgs;
 use hermes_cli_components::impls::commands::queries::client_state::QueryClientStateArgs;
 use hermes_cli_components::impls::commands::queries::consensus_state::QueryConsensusStateArgs;
 use hermes_cli_components::traits::command::{CanRunCommand, CommandRunner};
@@ -6,13 +7,16 @@ use hermes_cli_components::traits::command::{CanRunCommand, CommandRunner};
 pub enum QuerySubCommand {
     ClientState(QueryClientStateArgs),
     ConsensusState(QueryConsensusStateArgs),
+    ChainStatus(QueryChainStatusArgs),
 }
 
 pub struct RunQuerySubCommand;
 
 impl<App> CommandRunner<App, QuerySubCommand> for RunQuerySubCommand
 where
-    App: CanRunCommand<QueryClientStateArgs> + CanRunCommand<QueryConsensusStateArgs>,
+    App: CanRunCommand<QueryClientStateArgs>
+        + CanRunCommand<QueryConsensusStateArgs>
+        + CanRunCommand<QueryChainStatusArgs>,
 {
     async fn run_command(
         app: &App,
@@ -21,6 +25,7 @@ where
         match subcommand {
             QuerySubCommand::ClientState(args) => app.run_command(args).await,
             QuerySubCommand::ConsensusState(args) => app.run_command(args).await,
+            QuerySubCommand::ChainStatus(args) => app.run_command(args).await,
         }
     }
 }
