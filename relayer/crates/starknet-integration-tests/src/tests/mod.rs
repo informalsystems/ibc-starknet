9c59dcf34480cab255a02301ee63fb91608f5064
Ranadeep Biswas <mail@rnbguy.at>
2025-01-11 17:42:08 +0100

    poseidon hash impl

--- relayer/crates/starknet-integration-tests/src/tests/mod.rs
+++ relayer/crates/starknet-integration-tests/src/tests/mod.rs
@@ -1,4 +1,5 @@
 pub mod erc20;
 pub mod ics20;
 pub mod light_client;
+pub mod poseidon;
 pub mod update_clients;
---
+++ relayer/crates/starknet-integration-tests/src/tests/poseidon/hades.rs
@@ -0,0 +1,85 @@
+use starknet::core::types::Felt;
+
+// References:
+// https://github.com/starkware-libs/cairo-lang/blob/master/src/starkware/cairo/common/poseidon_utils.py
+
+pub struct HadesPermutate<
+    const DIM: usize,
+    const FULL_ROUNDS: usize,
+    const PARTIAL_ROUNDS: usize,
+    const N_ROUNDS: usize,
+> {
+    pub mds: [[i64; DIM]; DIM],
+    pub round_keys: [[&'static str; DIM]; N_ROUNDS],
+}
+
+impl<
+        const DIM: usize,
+        const FULL_ROUNDS: usize,
+        const PARTIAL_ROUNDS: usize,
+        const N_ROUNDS: usize,
+    > HadesPermutate<DIM, FULL_ROUNDS, PARTIAL_ROUNDS, N_ROUNDS>
+{
+    // Perform matrix multiplication in the field.
+    fn matrix_multiply(matrix: &[[i64; DIM]; DIM], vector: &[Felt; DIM]) -> [Felt; DIM] {
+        matrix.map(|row| {
+            row.iter()
+                .zip(vector.iter())
+                .map(|(&m, v)| Felt::from(m) * v)
+                .sum()
+        })
+    }
+
+    // Perform a single round of the Poseidon hash function.
+    fn hades_round(
+        &self,
+        values: [Felt; DIM],
+        is_full_round: bool,
+        round_idx: usize,
+    ) -> [Felt; DIM] {
+        // Add-Round Key
+        let mut values = core::array::from_fn(|i| {
+            values[i] + Felt::from_dec_str(self.round_keys[round_idx][i]).unwrap()
+        });
+
+        // Perform the cube operation (x^3) in the field.
+        fn cube(x: Felt) -> Felt {
+            x * x * x
+        }
+
+        // SubWords
+        if is_full_round {
+            values = values.map(cube);
+        } else {
+            values[DIM - 1] = cube(values[DIM - 1]);
+        }
+
+        // MixLayer
+        Self::matrix_multiply(&self.mds, &values)
+    }
+
+    // Perform the full Poseidon permutation.
+    pub fn hades_permutation(&self, mut values: [Felt; DIM]) -> [Felt; DIM] {
+        let mut round_idx = 0;
+
+        // Apply R_F/2 full rounds
+        for _ in 0..(FULL_ROUNDS / 2) {
+            values = self.hades_round(values, true, round_idx);
+            round_idx += 1;
+        }
+
+        // Apply R_P partial rounds
+        for _ in 0..PARTIAL_ROUNDS {
+            values = self.hades_round(values, false, round_idx);
+            round_idx += 1;
+        }
+
+        // Apply R_F/2 full rounds
+        for _ in 0..(FULL_ROUNDS / 2) {
+            values = self.hades_round(values, true, round_idx);
+            round_idx += 1;
+        }
+
+        values
+    }
+}
