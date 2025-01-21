8903152ecf71bf75b4c2fa9b17e0edd8062bff73
Farhad Shabani <Farhad.Shabani@gmail.com>
2025-01-21 12:03:18 -0800

    fix: update cargo lock + use CwClientValidation as trait bound

--- light-client/ibc-client-starknet/src/client_state/validation.rs
+++ light-client/ibc-client-starknet/src/client_state/validation.rs
@@ -1,5 +1,5 @@
+use ibc_client_cw::context::client_ctx::CwClientValidation;
 use ibc_core::client::context::client_state::ClientStateValidation;
-use ibc_core::client::context::ClientValidationContext;
 use ibc_core::client::types::error::ClientError;
 use ibc_core::client::types::Status;
 use ibc_core::host::types::identifiers::ClientId;
@@ -10,7 +10,7 @@ use crate::ConsensusState;
 
 impl<V> ClientStateValidation<V> for ClientState
 where
-    V: ClientValidationContext<ClientStateRef = ClientState, ConsensusStateRef = ConsensusState>,
+    V: CwClientValidation<ClientStateRef = ClientState, ConsensusStateRef = ConsensusState>,
 {
     fn verify_client_message(
         &self,
