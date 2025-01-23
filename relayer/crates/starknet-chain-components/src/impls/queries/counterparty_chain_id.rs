ef58c4380f2e666bae2e187bba209703a36dbdca
Soares Chen <soares.chen@maybevoid.com>
2025-01-23 16:29:26 +0000

    Implement QueryCosmosChainIdFromStarknetChannelId

--- relayer/crates/starknet-chain-components/src/impls/queries/counterparty_chain_id.rs
+++ relayer/crates/starknet-chain-components/src/impls/queries/counterparty_chain_id.rs
@@ -1,21 +1,59 @@
-use cgp::prelude::HasAsyncErrorType;
+use core::marker::PhantomData;
+
+use hermes_chain_components::traits::queries::chain_status::CanQueryChainHeight;
+use hermes_chain_components::traits::queries::channel_end::CanQueryChannelEnd;
+use hermes_chain_components::traits::queries::client_state::CanQueryClientState;
+use hermes_chain_components::traits::queries::connection_end::CanQueryConnectionEnd;
 use hermes_chain_components::traits::queries::counterparty_chain_id::CounterpartyChainIdQuerier;
-use hermes_chain_components::traits::types::chain_id::HasChainIdType;
-use hermes_chain_components::traits::types::ibc::{HasChannelIdType, HasPortIdType};
+use hermes_chain_components::traits::types::channel::HasChannelEndType;
+use hermes_chain_components::traits::types::client_state::HasClientStateFields;
+use hermes_chain_components::traits::types::connection::HasConnectionEndType;
+use hermes_chain_components::traits::types::ibc::{
+    HasChannelIdType, HasClientIdType, HasConnectionIdType, HasPortIdType,
+};
+
+use crate::types::channel_id::ChannelEnd;
+use crate::types::client_id::ClientId;
+use crate::types::connection_id::{ConnectionEnd, ConnectionId};
 
 pub struct QueryCosmosChainIdFromStarknetChannelId;
 
 impl<Chain, Counterparty> CounterpartyChainIdQuerier<Chain, Counterparty>
     for QueryCosmosChainIdFromStarknetChannelId
 where
-    Chain: HasChannelIdType<Counterparty> + HasPortIdType<Counterparty> + HasAsyncErrorType,
-    Counterparty: HasChainIdType,
+    Chain: HasChannelIdType<Counterparty>
+        + HasPortIdType<Counterparty>
+        + HasClientIdType<Counterparty, ClientId = ClientId>
+        + HasConnectionIdType<Counterparty, ConnectionId = ConnectionId>
+        + HasChannelEndType<Counterparty, ChannelEnd = ChannelEnd>
+        + HasConnectionEndType<Counterparty, ConnectionEnd = ConnectionEnd>
+        + CanQueryChainHeight
+        + CanQueryChannelEnd<Counterparty>
+        + CanQueryConnectionEnd<Counterparty>
+        + CanQueryClientState<Counterparty>,
+    Counterparty: HasClientStateFields<Chain>,
 {
     async fn query_counterparty_chain_id_from_channel_id(
-        _chain: &Chain,
-        _channel_id: &Chain::ChannelId,
-        _port_id: &Chain::PortId,
+        chain: &Chain,
+        channel_id: &Chain::ChannelId,
+        port_id: &Chain::PortId,
     ) -> Result<Counterparty::ChainId, Chain::Error> {
-        todo!()
+        let height = chain.query_chain_height().await?;
+
+        let channel_end = chain
+            .query_channel_end(channel_id, port_id, &height)
+            .await?;
+
+        let connection_end = chain
+            .query_connection_end(&channel_end.connection_id, &height)
+            .await?;
+
+        let client_state = chain
+            .query_client_state(PhantomData, &connection_end.client_id, &height)
+            .await?;
+
+        let chain_id = Counterparty::client_state_chain_id(&client_state);
+
+        Ok(chain_id)
     }
 }
