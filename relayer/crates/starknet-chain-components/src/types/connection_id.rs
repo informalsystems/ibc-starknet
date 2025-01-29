3f5231f88c2b9356d31c0136493e746de7a9e385
Ranadeep Biswas <mail@rnbguy.at>
2025-01-30 00:52:13 +0100

    use ibc-rs connection id

--- relayer/crates/starknet-chain-components/src/types/connection_id.rs
+++ relayer/crates/starknet-chain-components/src/types/connection_id.rs
@@ -1,45 +1,46 @@
-use core::fmt::{Display, Formatter};
-
 use cgp::core::component::UseContext;
 use cgp::prelude::*;
 use hermes_cairo_encoding_components::impls::encode_mut::variant_from::EncodeVariantFrom;
 use hermes_encoding_components::impls::encode_mut::combine::CombineEncoders;
 use hermes_encoding_components::impls::encode_mut::field::EncodeField;
 use hermes_encoding_components::impls::encode_mut::from::DecodeFrom;
+use hermes_encoding_components::traits::decode_mut::{CanDecodeMut, MutDecoder};
+use hermes_encoding_components::traits::encode_mut::{CanEncodeMut, MutEncoder};
 use hermes_encoding_components::traits::transform::{Transformer, TransformerRef};
 use hermes_wasm_encoding_components::components::{MutDecoderComponent, MutEncoderComponent};
+pub use ibc::core::host::types::identifiers::ConnectionId;
 
 use crate::types::client_id::ClientId;
 use crate::types::messages::ibc::connection::{BasePrefix, ConnectionVersion};
 
-#[derive(Debug, PartialEq, Clone, HasField)]
-pub struct ConnectionId {
-    pub connection_id: String,
-}
-
 pub struct EncodeConnectionId;
 
-delegate_components! {
-    EncodeConnectionId {
-        MutEncoderComponent: CombineEncoders<Product![
-            EncodeField<symbol!("connection_id"), UseContext>,
-        ]>,
-        MutDecoderComponent: DecodeFrom<Self, UseContext>,
+impl<Encoding, Strategy> MutEncoder<Encoding, Strategy, ConnectionId> for EncodeConnectionId
+where
+    Encoding: CanEncodeMut<Strategy, Product![String]>,
+{
+    fn encode_mut(
+        encoding: &Encoding,
+        value: &ConnectionId,
+        buffer: &mut Encoding::EncodeBuffer,
+    ) -> Result<(), Encoding::Error> {
+        encoding.encode_mut(&product![value.to_string()], buffer)?;
+        Ok(())
     }
 }
 
-impl Transformer for EncodeConnectionId {
-    type From = String;
-    type To = ConnectionId;
-
-    fn transform(connection_id: Self::From) -> ConnectionId {
-        ConnectionId { connection_id }
-    }
-}
-
-impl Display for ConnectionId {
-    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        self.connection_id.fmt(f)
+impl<Encoding, Strategy> MutDecoder<Encoding, Strategy, ConnectionId> for EncodeConnectionId
+where
+    Encoding: CanDecodeMut<Strategy, Product![String]> + CanRaiseAsyncError<&'static str>,
+{
+    fn decode_mut<'a>(
+        encoding: &Encoding,
+        buffer: &mut Encoding::DecodeBuffer<'a>,
+    ) -> Result<ConnectionId, Encoding::Error> {
+        let product![connection_id_str] = encoding.decode_mut(buffer)?;
+        connection_id_str
+            .parse()
+            .map_err(|_| Encoding::raise_error("invalid connection id"))
     }
 }
 
