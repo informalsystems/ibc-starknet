e84c218caf1808477a99c3c1529ccddff688bf00
Luca Joss <luca@informal.systems>
2025-01-30 13:40:31 +0100

    Add docstring to 'starknet transfer-args' command

--- tools/crates/cli/src/commands/starknet/subcommand.rs
+++ tools/crates/cli/src/commands/starknet/subcommand.rs
@@ -5,6 +5,9 @@ use crate::commands::starknet::transfer_args::TransferArgs;
 
 #[derive(Debug, clap::Subcommand)]
 pub enum StarknetSubCommand {
+    /// This command generates the arguments, `<args>` to pass to
+    /// `starkli invoke <contract addr> send_transfer <args>` in order
+    /// to trigger a token transfer
     TransferArgs(TransferArgs),
 }
 
