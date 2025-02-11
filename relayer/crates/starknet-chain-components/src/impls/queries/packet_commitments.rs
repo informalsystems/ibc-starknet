6bc0f185ed19dc329ceb9b1900d38796c19dd06c
Luca Joss <luca@informal.systems>
2025-02-11 09:40:58 +0100

    WIP: QueryStarknetPacketCommitments using events

---
+++ relayer/crates/starknet-chain-components/src/impls/queries/packet_commitments.rs
@@ -0,0 +1,123 @@
+use cgp::prelude::*;
+use hermes_cairo_encoding_components::strategy::ViaCairo;
+use hermes_cairo_encoding_components::types::as_felt::AsFelt;
+use hermes_chain_components::traits::commitment_prefix::HasIbcCommitmentPrefix;
+use hermes_chain_components::traits::queries::chain_status::CanQueryChainStatus;
+use hermes_chain_components::traits::queries::packet_commitments::PacketCommitmentsQuerier;
+use hermes_chain_components::traits::types::height::HasHeightType;
+use hermes_chain_components::traits::types::ibc::{
+    HasChannelIdType, HasPortIdType, HasSequenceType,
+};
+use hermes_chain_components::traits::types::packets::receive::HasPacketCommitmentType;
+use hermes_chain_components::traits::types::proof::HasCommitmentProofType;
+use hermes_cosmos_chain_components::types::key_types::secp256k1::Secp256k1KeyPair;
+use hermes_encoding_components::traits::decode::CanDecode;
+use hermes_encoding_components::traits::encode::CanEncode;
+use hermes_encoding_components::traits::has_encoding::HasEncoding;
+use hermes_encoding_components::traits::types::encoded::HasEncodedType;
+use ibc::core::host::types::identifiers::{PortId as IbcPortId, Sequence as IbcSequence};
+use pathfinder_rpc::context::RpcContext;
+use starknet::core::types::Felt;
+
+use crate::traits::contract::call::CanCallContract;
+use crate::traits::proof_signer::HasStarknetProofSigner;
+use crate::traits::queries::address::CanQueryContractAddress;
+use crate::traits::types::blob::HasBlobType;
+use crate::traits::types::method::HasSelectorType;
+use crate::types::channel_id::ChannelId;
+use crate::types::commitment_proof::StarknetCommitmentProof;
+use crate::types::messages::ibc::channel::PortId as CairoPortId;
+use crate::types::messages::ibc::packet::Sequence;
+use crate::types::status::StarknetChainStatus;
+
+pub struct QueryStarknetPacketCommitments;
+
+impl<Chain, Counterparty, Encoding> PacketCommitmentsQuerier<Chain, Counterparty>
+    for QueryStarknetPacketCommitments
+where
+    Chain: HasHeightType<Height = u64>
+        + CanQueryChainStatus<ChainStatus = StarknetChainStatus>
+        + HasIbcCommitmentPrefix<CommitmentPrefix = Vec<u8>>
+        + HasChannelIdType<Counterparty, ChannelId = ChannelId>
+        + HasPortIdType<Counterparty, PortId = IbcPortId>
+        + HasSequenceType<Counterparty, Sequence = IbcSequence>
+        + HasPacketCommitmentType<Counterparty, PacketCommitment = Vec<u8>>
+        + HasCommitmentProofType<CommitmentProof = StarknetCommitmentProof>
+        + HasBlobType<Blob = Vec<Felt>>
+        + HasSelectorType<Selector = Felt>
+        + CanQueryContractAddress<symbol!("ibc_core_contract_address")>
+        + HasEncoding<AsFelt, Encoding = Encoding>
+        + CanCallContract
+        + HasStarknetProofSigner<ProofSigner = Secp256k1KeyPair>
+        + CanRaiseAsyncError<String>
+        + CanRaiseAsyncError<Encoding::Error>,
+    Encoding: CanEncode<ViaCairo, Product![CairoPortId, ChannelId, Sequence]>
+        + CanDecode<ViaCairo, Product![[u32; 8]]>
+        + HasEncodedType<Encoded = Vec<Felt>>,
+{
+    async fn query_packet_commitments(
+        chain: &Chain,
+        channel_id: &ChannelId,
+        port_id: &IbcPortId,
+    ) -> Result<(Vec<IbcSequence>, u64), Chain::Error> {
+        let context = RpcContext::new();
+        let event_filter = GetEventsInput {
+            filter: EventFilter {
+                chunk_size: test_utils::NUM_EVENTS,
+                ..Default::default()
+            },
+        };
+        let get_events_result = get_events(context, event_filter)
+            .await
+            .map_errr(Chain::raise_error)?;
+
+        todo!();
+        /*let encoding = chain.encoding();
+
+        let contract_address = chain.query_contract_address(PhantomData).await?;
+
+        let calldata = encoding
+            .encode(&product![port_id.clone(), channel_id.clone(), *sequence])
+            .map_err(Chain::raise_error)?;
+
+        let output = chain
+            .call_contract(
+                &contract_address,
+                &selector!("packet_commitment"),
+                &calldata,
+            )
+            .await?;
+
+        let product![commitment,] = encoding.decode(&output).map_err(Chain::raise_error)?;
+
+        let commitment_bytes = commitment
+            .into_iter()
+            // TODO(rano): cairo uses [u32; 8], but in cosmos it's Vec<u8>
+            .flat_map(|felt| felt.to_be_bytes())
+            .collect::<Vec<_>>();
+
+        let chain_status = chain.query_chain_status().await?;
+
+        let unsigned_membership_proof_bytes = MembershipVerifierContainer {
+            state_root: chain_status.block_hash.to_bytes_be().to_vec(),
+            prefix: chain.ibc_commitment_prefix().clone(),
+            path: Path::Commitment(CommitmentPath::new(port_id, channel_id, *sequence))
+                .to_string()
+                .into(),
+            value: Some(commitment_bytes.clone()),
+        }
+        .canonical_bytes();
+
+        let signed_bytes = chain
+            .proof_signer()
+            .sign(&unsigned_membership_proof_bytes)
+            .map_err(Chain::raise_error)?;
+
+        let dummy_proof = StarknetCommitmentProof {
+            proof_height: chain_status.height,
+            proof_bytes: signed_bytes,
+        };
+
+        Ok((commitment_bytes, dummy_proof))*/
+    }
+}
