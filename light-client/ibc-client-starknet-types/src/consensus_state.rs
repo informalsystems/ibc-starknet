db99da74a2620d122df63e87ebbbc011ccd51fc8
Ranadeep Biswas <mail@rnbguy.at>
2025-02-19 18:00:56 +0100

    derive Eq

--- light-client/ibc-client-starknet-types/src/consensus_state.rs
+++ light-client/ibc-client-starknet-types/src/consensus_state.rs
@@ -5,7 +5,7 @@ use ibc_core::primitives::Timestamp;
 pub const STARKNET_CONSENSUS_STATE_TYPE_URL: &str = "/StarknetConsensusState";
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
-#[derive(Clone, Debug, PartialEq, derive_more::From, HasField)]
+#[derive(Clone, Debug, PartialEq, Eq, derive_more::From, HasField)]
 pub struct StarknetConsensusState {
     pub root: CommitmentRoot,
     pub time: Timestamp,
