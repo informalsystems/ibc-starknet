b9b13b10f9c10bab2edf0fbe38d7bf3ed79dc9d4
Soares Chen <soares.chen@maybevoid.com>
2025-02-20 09:38:21 +0000

    Update hermes-sdk

--- nix/ibc-starknet-cw.nix
+++ nix/ibc-starknet-cw.nix
@@ -10,7 +10,7 @@ let
     cargoLock = {
       lockFile = ./../light-client/Cargo.lock;
       outputHashes = {
-        "hermes-cosmos-encoding-components-0.1.0" = "sha256-dC7I3uegAQQ3eA1FkQaZ3AtxLARxw7/Vm1eiMo0I8N0=";
+        "hermes-cosmos-encoding-components-0.1.0" = "sha256-tqPlWLVfjDucmwjdmG316Jjy9bjAUo0WpDCzd+gcelo=";
         "cgp-0.3.1" = "sha256-sADWXKY/M73QqJasVicJWRTuuIMb0ZGIu1b8S8F/JG8=";
         "ibc-client-cw-0.56.0" = "sha256-DA3AB8ejUrx4ksBtN/vaOznjpKE0+0F6vGA7JmWyHWA=";
         "ibc-0.56.0" = "sha256-7DPIqu/zs0szjmtJTfXI2eQ0HEkRyvGjArcMZsFWMT4=";
