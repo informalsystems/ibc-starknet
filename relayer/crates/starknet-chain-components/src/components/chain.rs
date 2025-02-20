91b0285059d7bd574907c315911c47430b2e986d
Soares Chen <soares.chen@maybevoid.com>
2025-02-20 09:33:09 +0000

    Use generic QueryClearedPacketWithEmptyCommitment to check if packet is cleared

--- relayer/crates/starknet-chain-components/src/components/chain.rs
+++ relayer/crates/starknet-chain-components/src/components/chain.rs
@@ -79,7 +79,6 @@ mod preset {
     use crate::impls::queries::contract_address::GetContractAddressFromField;
     use crate::impls::queries::counterparty_chain_id::QueryCosmosChainIdFromStarknetChannelId;
     use crate::impls::queries::packet_commitment::QueryStarknetPacketCommitment;
-    use crate::impls::queries::packet_is_cleared::QueryStarknetPacketIsCleared;
     use crate::impls::queries::packet_receipt::QueryStarknetPacketReceipt;
     use crate::impls::queries::packet_received::QueryPacketIsReceivedOnStarknet;
     use crate::impls::queries::status::QueryStarknetChainStatus;
@@ -405,7 +404,7 @@ mod preset {
             PacketIsReceivedQuerierComponent:
                 QueryPacketIsReceivedOnStarknet,
             PacketIsClearedQuerierComponent:
-                QueryStarknetPacketIsCleared,
+                QueryClearedPacketWithEmptyCommitment,
         }
     }
 }
