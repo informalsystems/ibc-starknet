b0de8945212a64e6609043dea4069e347417f12c
Soares Chen <soares.chen@maybevoid.com>
2025-02-21 13:33:32 +0000

    Update Hermes SDK with logging improvements

--- relayer/crates/starknet-cli/bin/hermes_starknet.rs
+++ relayer/crates/starknet-cli/bin/hermes_starknet.rs
@@ -6,26 +6,16 @@ use std::sync::Arc;
 use clap::Parser;
 use eyre::{eyre, Error};
 use hermes_cli_components::traits::command::CanRunCommand;
+use hermes_logger::subscriber::init_tracing_subscriber;
 use hermes_runtime::types::runtime::HermesRuntime;
 use hermes_starknet_cli::commands::all::AllSubCommands;
 use hermes_starknet_cli::contexts::app::StarknetApp;
 use tokio::runtime::Builder;
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::util::SubscriberInitExt;
-use tracing_subscriber::{fmt, EnvFilter};
 
 fn main() {
     let _ = stable_eyre::install();
 
-    let env_filter = EnvFilter::builder()
-        .with_default_directive(LevelFilter::INFO.into())
-        .from_env_lossy();
-
-    tracing_subscriber::registry()
-        .with(fmt::layer())
-        .with(env_filter)
-        .init();
+    init_tracing_subscriber();
 
     let res = run_main();
 
