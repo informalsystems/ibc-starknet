bd0827deb810aa1ad26c980f507b411b18ac9b04
Ranadeep Biswas <mail@rnbguy.at>
2025-02-28 12:14:34 +0100

    use enum variant

--- relayer/crates/starknet-chain-components/src/types/wallet.rs
+++ relayer/crates/starknet-chain-components/src/types/wallet.rs
@@ -4,8 +4,35 @@ use starknet::core::types::Felt;
 use crate::impls::types::address::StarknetAddress;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SigningKey {
+    File(String),
+    Felt(Felt),
+}
+
+impl TryFrom<SigningKey> for Felt {
+    type Error = String;
+
+    fn try_from(value: SigningKey) -> Result<Self, Self::Error> {
+        match value {
+            SigningKey::File(path) => Ok(std::fs::read_to_string(path)
+                .map_err(|e| format!("{e}"))?
+                .trim()
+                .parse()
+                .map_err(|e| format!("{e}"))?),
+            SigningKey::Felt(felt) => Ok(felt),
+        }
+    }
+}
+
+impl From<Felt> for SigningKey {
+    fn from(felt: Felt) -> Self {
+        Self::Felt(felt)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct StarknetWallet {
     pub account_address: StarknetAddress,
-    pub signing_key: Felt,
+    pub signing_key: SigningKey,
     pub public_key: Felt,
 }
