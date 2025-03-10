468c33d56460c2fddde720d07a3862df1101ff45
Ranadeep Biswas <mail@rnbguy.at>
2025-03-10 19:49:14 +0100

    deno scripts to query ibc states and token addresses

---
+++ scripts/starknet_ibc_core.ts
@@ -0,0 +1,182 @@
+#!/usr/bin/env -S deno run
+
+import { Contract, RpcProvider } from "npm:starknet";
+import { QueryClient, setupIbcExtension } from "npm:@cosmjs/stargate";
+import { Tendermint34Client } from "npm:@cosmjs/tendermint-rpc";
+
+const OSMOSIS_RPC_ENDPOINT = "https://rpc.testnet.osmosis.zone";
+const STARKNET_RPC_ENDPOINT =
+    "https://starknet-sepolia.public.blastapi.io/rpc/v0_7";
+const IBC_CORE_CONTRACT =
+    "0x05083221904c1a0c9fa646230f54c9e130e498cdfa4ce8f7e82b63384893262d";
+const IBC_CLIENT_CONTRACT =
+    "0x007b3e3b54558145bedc6c9eb50bb27cb9b0a84973e1644d9f58be0d2be6b0b8";
+const CHANNEL_ID = "channel-2";
+const PORT_ID = "transfer";
+
+const starknetProvider = new RpcProvider({ nodeUrl: STARKNET_RPC_ENDPOINT });
+
+async function queryStarknetIbc() {
+    console.log("Querying Starknet IBC data...");
+
+    const ibcCoreClass = await starknetProvider.getClassAt(IBC_CORE_CONTRACT);
+    const ibcClientClass = await starknetProvider.getClassAt(
+        IBC_CLIENT_CONTRACT,
+    );
+
+    const ibcCoreContract = new Contract(
+        ibcCoreClass.abi,
+        IBC_CORE_CONTRACT,
+        starknetProvider,
+    );
+    const ibcClientContract = new Contract(
+        ibcClientClass.abi,
+        IBC_CLIENT_CONTRACT,
+        starknetProvider,
+    );
+
+    const channelEnd = await ibcCoreContract.channel_end({
+        port_id: PORT_ID,
+    }, {
+        channel_id: CHANNEL_ID,
+    });
+
+    const connectionId = channelEnd.connection_id;
+
+    const connectionEnd = await ibcCoreContract.connection_end(
+        connectionId,
+    );
+
+    const clientId = connectionEnd.client_id;
+    const clientSequence = clientId.sequence;
+
+    const clientType = await ibcClientContract.client_type();
+    const clientLatestHeight = await ibcClientContract.latest_height(
+        clientSequence,
+    );
+    const clientLatestTimestamp = await ibcClientContract.latest_timestamp(
+        clientSequence,
+    );
+    const clientStatus = await ibcClientContract.status(clientSequence);
+    const clientState = await ibcClientContract.client_state(
+        clientSequence,
+    );
+    const consensusState = await ibcClientContract.consensus_state(
+        clientSequence,
+        clientLatestHeight,
+    );
+    const consensusStateRoot = await ibcClientContract
+        .consensus_state_root(
+            clientSequence,
+            clientLatestHeight,
+        );
+
+    return {
+        portId: PORT_ID,
+        channelId: CHANNEL_ID,
+        connectionId: connectionId,
+        clientId: clientId,
+        clientSequence: clientSequence,
+
+        channelEnd: channelEnd,
+        connectionEnd: connectionEnd,
+        clientType: clientType,
+        latestHeight: clientLatestHeight,
+        latestTimestamp: clientLatestTimestamp,
+        clientStatus: clientStatus,
+        clientState: clientState,
+        consensusState: consensusState,
+        consensusStateRoot: consensusStateRoot,
+    };
+}
+
+async function queryOsmosisIbc(
+    portId: string,
+    channelId: string,
+) {
+    console.log("Querying Osmosis IBC data...");
+
+    const tmClient = await Tendermint34Client.connect(OSMOSIS_RPC_ENDPOINT);
+    const queryClient = QueryClient.withExtensions(tmClient, setupIbcExtension);
+
+    const channelEnd = await queryClient.ibc.channel.channel(
+        portId,
+        channelId,
+    );
+
+    const connectionId = channelEnd.channel!.connectionHops[0];
+
+    const connectionEnd = await queryClient.ibc.connection.connection(
+        connectionId,
+    );
+
+    const clientId = connectionEnd.connection!.clientId!;
+    const clientState = await queryClient.ibc.client.state(
+        clientId!,
+    );
+
+    return {
+        portId: portId,
+        channelId: channelId,
+        connectionId: connectionId,
+        clientId: clientId,
+
+        channelEnd: channelEnd,
+        connectionEnd: connectionEnd,
+        clientState: clientState,
+    };
+}
+
+const starknetData = await queryStarknetIbc();
+
+const osmosisData = await queryOsmosisIbc(
+    starknetData.channelEnd.remote.port_id.port_id,
+    starknetData.channelEnd.remote.channel_id.channel_id,
+);
+
+console.log("\n=== Starknet IBC Data ===");
+
+console.log("Channel:");
+console.log(`  Port ID:`, starknetData.portId);
+console.log(`  Channel ID:`, starknetData.channelId);
+console.log("Channel End Details:");
+console.log(starknetData.channelEnd);
+
+console.log("\nConnection:");
+console.log(`  Connection ID:`, starknetData.connectionId);
+console.log("Connection End Details:");
+console.log(starknetData.connectionEnd);
+
+console.log("\nClient:");
+console.log(`  Client ID:`, starknetData.clientId);
+console.log(`  Client Sequence:`, starknetData.clientSequence);
+console.log(`  Client Type:`, starknetData.clientType);
+console.log(`  Latest Height:`, starknetData.latestHeight);
+console.log(`  Latest Timestamp:`, starknetData.latestTimestamp);
+console.log(`  Status:`, starknetData.clientStatus);
+console.log("Client State Details:");
+console.log(starknetData.clientState);
+
+console.log("\nConsensus State:");
+console.log(starknetData.consensusState);
+
+console.log("\nConsensus State Root:");
+console.log(starknetData.consensusStateRoot);
+
+console.log("\n=== Osmosis IBC Data ===");
+
+console.log("\nChannel:");
+console.log(`  Port ID:`, osmosisData.portId);
+console.log(`  Channel ID:`, osmosisData.channelId);
+console.log("Channel End Details:");
+console.log(osmosisData.channelEnd);
+
+console.log("\nConnection:");
+console.log(`  Connection ID:`, osmosisData.connectionId);
+console.log("Connection End Details:");
+console.log(osmosisData.connectionEnd);
+
+console.log("Client:");
+console.log(`  Client ID:`, osmosisData.clientId);
+console.log("Client State Details:");
+console.log(osmosisData.clientState);
---
+++ scripts/starknet_ibc_denom.ts
@@ -0,0 +1,95 @@
+#!/usr/bin/env -S deno run
+
+import {
+    byteArray,
+    CairoCustomEnum,
+    CallData,
+    Contract,
+    hash,
+    num,
+    RpcProvider,
+} from "npm:starknet";
+
+const STARKNET_RPC_ENDPOINT =
+    "https://starknet-sepolia.public.blastapi.io/rpc/v0_7";
+const ICS20_CONTRACT =
+    "0x01bc3df6b90ea052fa965fceb983277f8bf1bc7d3484f80c67c219915c72c92e";
+
+const CHANNEL_ID = "channel-2";
+const PORT_ID = "transfer";
+const OSMO_DENOM = "uosmo";
+
+const starknetProvider = new RpcProvider({ nodeUrl: STARKNET_RPC_ENDPOINT });
+
+const ics20Class = await starknetProvider.getClassAt(ICS20_CONTRACT);
+const ics20Contract = new Contract(
+    ics20Class.abi,
+    ICS20_CONTRACT,
+    starknetProvider,
+);
+
+const ibc_prefixed_denom = {
+    trace_path: [{
+        port_id: {
+            port_id: byteArray.byteArrayFromString(PORT_ID),
+        },
+        channel_id: {
+            channel_id: byteArray.byteArrayFromString(CHANNEL_ID),
+        },
+    }],
+    base: new CairoCustomEnum({
+        Native: undefined,
+        Hosted: byteArray.byteArrayFromString(OSMO_DENOM),
+    }),
+};
+
+{
+    const serialized_ibc_prefixed_denom = CallData.compile(ibc_prefixed_denom);
+
+    const callDataAbi = new CallData(
+        ics20Contract.abi,
+    );
+
+    const deserializedPrefixedDenom = callDataAbi.decodeParameters(
+        "starknet_ibc_apps::transfer::types::PrefixedDenom",
+        serialized_ibc_prefixed_denom,
+    );
+
+    console.log(deserializedPrefixedDenom);
+}
+
+const serialized_ibc_prefixed_denom = [];
+
+// transfer/channel-0/transfer/channel-23/coin
+// compile(transfer/channel-0):compile(transfer/channel-23):compile(coin)
+for (const trace_prefix of ibc_prefixed_denom.trace_path.slice().reverse()) {
+    serialized_ibc_prefixed_denom.push(...CallData.compile(trace_prefix));
+}
+
+serialized_ibc_prefixed_denom.push(
+    ...CallData.compile({ base: ibc_prefixed_denom.base }),
+);
+
+const ibc_prefixed_denom_key = hash.computePoseidonHashOnElements(
+    serialized_ibc_prefixed_denom,
+);
+
+const ibc_token_address = num.toHex(
+    await ics20Contract.ibc_token_address(
+        ibc_prefixed_denom_key,
+    ),
+);
+
+console.log("ERC20 token address:", ibc_token_address);
+
+const ics20TokenClass = await starknetProvider.getClassAt(ibc_token_address);
+const ics20TokenContract = new Contract(
+    ics20TokenClass.abi,
+    ibc_token_address,
+    starknetProvider,
+);
+
+console.log("  name:", await ics20TokenContract.name());
+console.log("  symbol:", await ics20TokenContract.symbol());
+console.log("  decimals:", await ics20TokenContract.decimals());
+console.log("  total_supply:", await ics20TokenContract.total_supply());
