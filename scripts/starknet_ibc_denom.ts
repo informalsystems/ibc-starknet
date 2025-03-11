a747eaa514068c76ac9035b477fe83b8cd0c4a3d
Ranadeep Biswas <mail@rnbguy.at>
2025-03-11 18:26:14 +0100

    format deno scripts

--- scripts/starknet_ibc_denom.ts
+++ scripts/starknet_ibc_denom.ts
@@ -1,19 +1,19 @@
 #!/usr/bin/env -S deno run
 
 import {
-    byteArray,
-    CairoCustomEnum,
-    CallData,
-    Contract,
-    hash,
-    num,
-    RpcProvider,
+  byteArray,
+  CairoCustomEnum,
+  CallData,
+  Contract,
+  hash,
+  num,
+  RpcProvider,
 } from "npm:starknet";
 
 const STARKNET_RPC_ENDPOINT =
-    "https://starknet-sepolia.public.blastapi.io/rpc/v0_7";
+  "https://starknet-sepolia.public.blastapi.io/rpc/v0_7";
 const ICS20_CONTRACT =
-    "0x01bc3df6b90ea052fa965fceb983277f8bf1bc7d3484f80c67c219915c72c92e";
+  "0x01bc3df6b90ea052fa965fceb983277f8bf1bc7d3484f80c67c219915c72c92e";
 
 const CHANNEL_ID = "channel-2";
 const PORT_ID = "transfer";
@@ -23,39 +23,39 @@ const starknetProvider = new RpcProvider({ nodeUrl: STARKNET_RPC_ENDPOINT });
 
 const ics20Class = await starknetProvider.getClassAt(ICS20_CONTRACT);
 const ics20Contract = new Contract(
-    ics20Class.abi,
-    ICS20_CONTRACT,
-    starknetProvider,
+  ics20Class.abi,
+  ICS20_CONTRACT,
+  starknetProvider,
 );
 
 const ibc_prefixed_denom = {
-    trace_path: [{
-        port_id: {
-            port_id: byteArray.byteArrayFromString(PORT_ID),
-        },
-        channel_id: {
-            channel_id: byteArray.byteArrayFromString(CHANNEL_ID),
-        },
-    }],
-    base: new CairoCustomEnum({
-        Native: undefined,
-        Hosted: byteArray.byteArrayFromString(OSMO_DENOM),
-    }),
+  trace_path: [{
+    port_id: {
+      port_id: byteArray.byteArrayFromString(PORT_ID),
+    },
+    channel_id: {
+      channel_id: byteArray.byteArrayFromString(CHANNEL_ID),
+    },
+  }],
+  base: new CairoCustomEnum({
+    Native: undefined,
+    Hosted: byteArray.byteArrayFromString(OSMO_DENOM),
+  }),
 };
 
 {
-    const serialized_ibc_prefixed_denom = CallData.compile(ibc_prefixed_denom);
+  const serialized_ibc_prefixed_denom = CallData.compile(ibc_prefixed_denom);
 
-    const callDataAbi = new CallData(
-        ics20Contract.abi,
-    );
+  const callDataAbi = new CallData(
+    ics20Contract.abi,
+  );
 
-    const deserializedPrefixedDenom = callDataAbi.decodeParameters(
-        "starknet_ibc_apps::transfer::types::PrefixedDenom",
-        serialized_ibc_prefixed_denom,
-    );
+  const deserializedPrefixedDenom = callDataAbi.decodeParameters(
+    "starknet_ibc_apps::transfer::types::PrefixedDenom",
+    serialized_ibc_prefixed_denom,
+  );
 
-    console.log(deserializedPrefixedDenom);
+  console.log(deserializedPrefixedDenom);
 }
 
 const serialized_ibc_prefixed_denom = [];
@@ -63,30 +63,30 @@ const serialized_ibc_prefixed_denom = [];
 // transfer/channel-0/transfer/channel-23/coin
 // compile(transfer/channel-0):compile(transfer/channel-23):compile(coin)
 for (const trace_prefix of ibc_prefixed_denom.trace_path.slice().reverse()) {
-    serialized_ibc_prefixed_denom.push(...CallData.compile(trace_prefix));
+  serialized_ibc_prefixed_denom.push(...CallData.compile(trace_prefix));
 }
 
 serialized_ibc_prefixed_denom.push(
-    ...CallData.compile({ base: ibc_prefixed_denom.base }),
+  ...CallData.compile({ base: ibc_prefixed_denom.base }),
 );
 
 const ibc_prefixed_denom_key = hash.computePoseidonHashOnElements(
-    serialized_ibc_prefixed_denom,
+  serialized_ibc_prefixed_denom,
 );
 
 const ibc_token_address = num.toHex(
-    await ics20Contract.ibc_token_address(
-        ibc_prefixed_denom_key,
-    ),
+  await ics20Contract.ibc_token_address(
+    ibc_prefixed_denom_key,
+  ),
 );
 
 console.log("ERC20 token address:", ibc_token_address);
 
 const ics20TokenClass = await starknetProvider.getClassAt(ibc_token_address);
 const ics20TokenContract = new Contract(
-    ics20TokenClass.abi,
-    ibc_token_address,
-    starknetProvider,
+  ics20TokenClass.abi,
+  ibc_token_address,
+  starknetProvider,
 );
 
 console.log("  name:", await ics20TokenContract.name());
