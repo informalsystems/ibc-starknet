c8f1149c5b7e190d878c6cb627bf68b59c4c3951
Ranadeep Biswas <mail@rnbguy.at>
2025-03-11 20:33:55 +0100

    use hermes runtime

--- relayer/crates/starknet-cli/examples/bootstrap_ibc.rs
+++ relayer/crates/starknet-cli/examples/bootstrap_ibc.rs
@@ -55,8 +55,7 @@ pub const STARKNET_ETH: Felt =
 pub const WASM_CODE_HASH_HEX: &str =
     "6be4d4cbb85ea2d7e0b17b7053e613af11e041617bdb163107dfd29f706318ef";
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() -> Result<(), Error> {
     let _ = stable_eyre::install();
 
     let subscriber = tracing_subscriber::FmtSubscriber::builder()
@@ -73,242 +72,253 @@ async fn main() -> Result<(), Error> {
 
     info!("initialized Hermes runtime");
 
-    let (starknet_chain, cosmos_chain) = {
-        let starknet_app = StarknetApp {
-            config_path: config_file.into(),
-            runtime: runtime.clone(),
-        };
+    runtime.runtime.block_on(async {
+        let (starknet_chain, cosmos_chain) = {
+            let starknet_app = StarknetApp {
+                config_path: config_file.into(),
+                runtime: runtime.clone(),
+            };
 
-        let starknet_builder = starknet_app.load_builder().await?;
+            let starknet_builder = starknet_app.load_builder().await?;
 
-        let starknet_chain: StarknetChain = starknet_builder
-            .build_chain(&STARKNET_TESTNET_CHAIN_ID.to_string().parse()?)
-            .await
-            .map_err(|e| eyre::eyre!("failed to build starknet chain: {:?}", e))?;
-
-        info!(
-            "starknet_chain_status: {:?}",
-            starknet_chain
-                .query_chain_status()
+            let starknet_chain: StarknetChain = starknet_builder
+                .build_chain(&STARKNET_TESTNET_CHAIN_ID.to_string().parse()?)
                 .await
-                .map_err(|e| eyre::eyre!("failed to query starknet chain status: {:?}", e))?
-        );
+                .map_err(|e| eyre::eyre!("failed to build starknet chain: {:?}", e))?;
 
-        let cosmos_chain: CosmosChain = starknet_builder
-            .cosmos_builder
-            .build_chain(&OSMOSIS_TESTNET_CHAIN_ID.parse()?)
-            .await?;
+            info!(
+                "starknet_chain_status: {:?}",
+                starknet_chain
+                    .query_chain_status()
+                    .await
+                    .map_err(|e| eyre::eyre!("failed to query starknet chain status: {:?}", e))?
+            );
 
-        info!(
-            "cosmos_chain_status: {:?}",
-            cosmos_chain.query_chain_status().await?
-        );
+            let cosmos_chain: CosmosChain = starknet_builder
+                .cosmos_builder
+                .build_chain(&OSMOSIS_TESTNET_CHAIN_ID.parse()?)
+                .await?;
 
-        (starknet_chain, cosmos_chain)
-    };
+            info!(
+                "cosmos_chain_status: {:?}",
+                cosmos_chain.query_chain_status().await?
+            );
 
-    let starknet_client_id = {
-        // https://lcd.testnet.osmosis.zone/cosmos/staking/v1beta1/params
-        // in seconds; 5 days.
-        let osmosis_unbonding_period = 432000;
-
-        // https://docs.starknet.io/chain-info/#current_limits
-        // in seconds.
-        let starknet_block_time = 30;
-
-        StarknetToCosmosRelay::create_client(
-            SourceTarget,
-            &starknet_chain,
-            &cosmos_chain,
-            &CosmosCreateClientOptions {
-                // unbonding period is 5 days on osmo-test-5
-                //
-                // using (unbonading period - 1) as maximum allowed value.
-                trusting_period: Duration::from_secs(osmosis_unbonding_period - 1),
-                // starknet has 30 seconds block time
-                // block timestamp is when the sequencer started building the block
-                // which can be in the past
-                //
-                // using 5 mins as max clock drift to be more permissive.
-                max_clock_drift: Duration::from_secs(starknet_block_time * 10),
-
-                ..Default::default()
-            },
-            &(),
-        )
-        .await?
-    };
-
-    let cosmos_client_id = {
-        StarknetToCosmosRelay::create_client(
-            DestinationTarget,
-            &cosmos_chain,
-            &starknet_chain,
-            &StarknetCreateClientPayloadOptions {
-                wasm_code_hash: <[u8; 32]>::from_hex(WASM_CODE_HASH_HEX).expect("valid hex"),
-            },
-            &(),
-        )
-        .await?
-    };
-
-    {
-        info!("created client on Starknet: {:?}", starknet_client_id);
-
-        let client_state_on_starknet = starknet_chain
-            .query_client_state_with_latest_height(PhantomData::<CosmosChain>, &starknet_client_id)
-            .await?;
-
-        info!(
-            "client state on Starknet: {} => {:?}",
-            starknet_client_id, client_state_on_starknet
-        );
-
-        assert_eq!(&client_state_on_starknet.chain_id, cosmos_chain.chain_id());
-    }
-
-    {
-        info!("created client on Cosmos: {:?}", cosmos_client_id);
-
-        let client_state_on_cosmos = cosmos_chain
-            .query_client_state_with_latest_height(PhantomData::<StarknetChain>, &cosmos_client_id)
-            .await?;
-
-        info!(
-            "client state on Cosmos: {} => {:?}",
-            cosmos_client_id, client_state_on_cosmos
-        );
-
-        assert_eq!(
-            &client_state_on_cosmos.client_state.chain_id,
-            starknet_chain.chain_id()
-        );
-    }
-
-    let starknet_to_cosmos_relay = StarknetToCosmosRelay::new(
-        runtime.clone(),
-        starknet_chain.clone(),
-        cosmos_chain.clone(),
-        starknet_client_id.clone(),
-        cosmos_client_id.clone(),
-    );
-
-    let cosmos_to_starknet_relay = CosmosToStarknetRelay::new(
-        runtime.clone(),
-        cosmos_chain.clone(),
-        starknet_chain.clone(),
-        cosmos_client_id.clone(),
-        starknet_client_id.clone(),
-    );
-
-    {
-        let starknet_to_cosmos_relay = starknet_to_cosmos_relay.clone();
-
-        let cosmos_to_starknet_relay = cosmos_to_starknet_relay.clone();
-
-        runtime.runtime.spawn(async move {
-            let _ = starknet_to_cosmos_relay.run().await;
-        });
-
-        runtime.runtime.spawn(async move {
-            let _ = cosmos_to_starknet_relay.run().await;
-        });
-    }
-
-    let (starknet_connection_id, cosmos_connection_id) = {
-        let conn_init_option = CosmosInitConnectionOptions {
-            delay_period: Duration::from_secs(0),
-            connection_version: IbcConnectionVersion::compatibles().first().unwrap().clone(),
+            (starknet_chain, cosmos_chain)
         };
 
-        starknet_to_cosmos_relay
-            .bootstrap_connection(&conn_init_option)
+        let starknet_client_id = {
+            // https://lcd.testnet.osmosis.zone/cosmos/staking/v1beta1/params
+            // in seconds; 5 days.
+            let osmosis_unbonding_period = 432000;
+
+            // https://docs.starknet.io/chain-info/#current_limits
+            // in seconds.
+            let starknet_block_time = 30;
+
+            StarknetToCosmosRelay::create_client(
+                SourceTarget,
+                &starknet_chain,
+                &cosmos_chain,
+                &CosmosCreateClientOptions {
+                    // unbonding period is 5 days on osmo-test-5
+                    //
+                    // using (unbonading period - 1) as maximum allowed value.
+                    trusting_period: Duration::from_secs(osmosis_unbonding_period - 1),
+                    // starknet has 30 seconds block time
+                    // block timestamp is when the sequencer started building the block
+                    // which can be in the past
+                    //
+                    // using 5 mins as max clock drift to be more permissive.
+                    max_clock_drift: Duration::from_secs(starknet_block_time * 10),
+
+                    ..Default::default()
+                },
+                &(),
+            )
             .await?
-    };
+        };
 
-    {
-        info!(
-            "created connection: {:?}(Starknet) <> {:?}(Osmosis)",
-            starknet_connection_id, cosmos_connection_id
-        );
-
-        let starknet_connection_end = CanQueryConnectionEnd::<CosmosChain>::query_connection_end(
-            &starknet_chain,
-            &starknet_connection_id,
-            &starknet_chain.query_chain_height().await?,
-        )
-        .await?;
-
-        info!(
-            "starknet_connection: {:?} => {:?}",
-            starknet_connection_id, starknet_connection_end
-        );
-
-        let cosmos_connection_end = CanQueryConnectionEnd::<StarknetChain>::query_connection_end(
-            &cosmos_chain,
-            &cosmos_connection_id,
-            &cosmos_chain.query_chain_height().await?,
-        )
-        .await?;
-
-        info!(
-            "cosmos_connection: {:?} => {:?}",
-            cosmos_connection_id, cosmos_connection_end
-        );
-    }
-
-    let (starknet_channel_id, cosmos_channel_id) = {
-        let init_channel_options = CosmosInitChannelOptions::new(starknet_connection_id.clone());
-
-        let ics20_port = PortId::transfer();
-
-        starknet_to_cosmos_relay
-            .bootstrap_channel(&ics20_port, &ics20_port, &init_channel_options)
+        let cosmos_client_id = {
+            StarknetToCosmosRelay::create_client(
+                DestinationTarget,
+                &cosmos_chain,
+                &starknet_chain,
+                &StarknetCreateClientPayloadOptions {
+                    wasm_code_hash: <[u8; 32]>::from_hex(WASM_CODE_HASH_HEX).expect("valid hex"),
+                },
+                &(),
+            )
             .await?
-    };
+        };
 
-    {
-        info!(
-            "created channel: {:?}(Starknet) <> {:?}(Osmosis)",
-            starknet_channel_id, cosmos_channel_id
+        {
+            info!("created client on Starknet: {:?}", starknet_client_id);
+
+            let client_state_on_starknet = starknet_chain
+                .query_client_state_with_latest_height(
+                    PhantomData::<CosmosChain>,
+                    &starknet_client_id,
+                )
+                .await?;
+
+            info!(
+                "client state on Starknet: {} => {:?}",
+                starknet_client_id, client_state_on_starknet
+            );
+
+            assert_eq!(&client_state_on_starknet.chain_id, cosmos_chain.chain_id());
+        }
+
+        {
+            info!("created client on Cosmos: {:?}", cosmos_client_id);
+
+            let client_state_on_cosmos = cosmos_chain
+                .query_client_state_with_latest_height(
+                    PhantomData::<StarknetChain>,
+                    &cosmos_client_id,
+                )
+                .await?;
+
+            info!(
+                "client state on Cosmos: {} => {:?}",
+                cosmos_client_id, client_state_on_cosmos
+            );
+
+            assert_eq!(
+                &client_state_on_cosmos.client_state.chain_id,
+                starknet_chain.chain_id()
+            );
+        }
+
+        let starknet_to_cosmos_relay = StarknetToCosmosRelay::new(
+            runtime.clone(),
+            starknet_chain.clone(),
+            cosmos_chain.clone(),
+            starknet_client_id.clone(),
+            cosmos_client_id.clone(),
         );
 
-        let starknet_channel_end = CanQueryChannelEnd::<CosmosChain>::query_channel_end(
-            &starknet_chain,
-            &starknet_channel_id,
-            &PortId::transfer(),
-            &starknet_chain.query_chain_height().await?,
-        )
-        .await?;
-
-        info!(
-            "starknet_channel: {:?} => {:?}",
-            starknet_channel_id, starknet_channel_end
+        let cosmos_to_starknet_relay = CosmosToStarknetRelay::new(
+            runtime.clone(),
+            cosmos_chain.clone(),
+            starknet_chain.clone(),
+            cosmos_client_id.clone(),
+            starknet_client_id.clone(),
         );
 
-        let cosmos_channel_end = CanQueryChannelEnd::<StarknetChain>::query_channel_end(
-            &cosmos_chain,
-            &cosmos_channel_id,
-            &PortId::transfer(),
-            &cosmos_chain.query_chain_height().await?,
-        )
-        .await?;
+        {
+            let starknet_to_cosmos_relay = starknet_to_cosmos_relay.clone();
+
+            let cosmos_to_starknet_relay = cosmos_to_starknet_relay.clone();
+
+            runtime.runtime.spawn(async move {
+                let _ = starknet_to_cosmos_relay.run().await;
+            });
+
+            runtime.runtime.spawn(async move {
+                let _ = cosmos_to_starknet_relay.run().await;
+            });
+        }
+
+        let (starknet_connection_id, cosmos_connection_id) = {
+            let conn_init_option = CosmosInitConnectionOptions {
+                delay_period: Duration::from_secs(0),
+                connection_version: IbcConnectionVersion::compatibles().first().unwrap().clone(),
+            };
+
+            starknet_to_cosmos_relay
+                .bootstrap_connection(&conn_init_option)
+                .await?
+        };
+
+        {
+            info!(
+                "created connection: {:?}(Starknet) <> {:?}(Osmosis)",
+                starknet_connection_id, cosmos_connection_id
+            );
+
+            let starknet_connection_end =
+                CanQueryConnectionEnd::<CosmosChain>::query_connection_end(
+                    &starknet_chain,
+                    &starknet_connection_id,
+                    &starknet_chain.query_chain_height().await?,
+                )
+                .await?;
+
+            info!(
+                "starknet_connection: {:?} => {:?}",
+                starknet_connection_id, starknet_connection_end
+            );
+
+            let cosmos_connection_end =
+                CanQueryConnectionEnd::<StarknetChain>::query_connection_end(
+                    &cosmos_chain,
+                    &cosmos_connection_id,
+                    &cosmos_chain.query_chain_height().await?,
+                )
+                .await?;
+
+            info!(
+                "cosmos_connection: {:?} => {:?}",
+                cosmos_connection_id, cosmos_connection_end
+            );
+        }
+
+        let (starknet_channel_id, cosmos_channel_id) = {
+            let init_channel_options =
+                CosmosInitChannelOptions::new(starknet_connection_id.clone());
+
+            let ics20_port = PortId::transfer();
+
+            starknet_to_cosmos_relay
+                .bootstrap_channel(&ics20_port, &ics20_port, &init_channel_options)
+                .await?
+        };
+
+        {
+            info!(
+                "created channel: {:?}(Starknet) <> {:?}(Osmosis)",
+                starknet_channel_id, cosmos_channel_id
+            );
+
+            let starknet_channel_end = CanQueryChannelEnd::<CosmosChain>::query_channel_end(
+                &starknet_chain,
+                &starknet_channel_id,
+                &PortId::transfer(),
+                &starknet_chain.query_chain_height().await?,
+            )
+            .await?;
+
+            info!(
+                "starknet_channel: {:?} => {:?}",
+                starknet_channel_id, starknet_channel_end
+            );
+
+            let cosmos_channel_end = CanQueryChannelEnd::<StarknetChain>::query_channel_end(
+                &cosmos_chain,
+                &cosmos_channel_id,
+                &PortId::transfer(),
+                &cosmos_chain.query_chain_height().await?,
+            )
+            .await?;
+
+            info!(
+                "cosmos_channel: {:?} => {:?}",
+                cosmos_channel_id, cosmos_channel_end
+            );
+        }
 
         info!(
-            "cosmos_channel: {:?} => {:?}",
-            cosmos_channel_id, cosmos_channel_end
+            "Osmosis: {}, {}, {}",
+            cosmos_client_id, cosmos_connection_id, cosmos_channel_id
         );
-    }
 
-    info!(
-        "Osmosis: {}, {}, {}",
-        cosmos_client_id, cosmos_connection_id, cosmos_channel_id
-    );
+        info!(
+            "Starknet: {}, {}, {}",
+            starknet_client_id, starknet_connection_id, starknet_channel_id
+        );
 
-    info!(
-        "Starknet: {}, {}, {}",
-        starknet_client_id, starknet_connection_id, starknet_channel_id
-    );
-
-    Ok(())
+        Ok(())
+    })
 }
