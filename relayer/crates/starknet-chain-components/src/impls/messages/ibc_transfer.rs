ee0e03401b08f56459df75819c38b023a91e648d
Soares Chen <soares.chen@maybevoid.com>
2025-03-13 11:02:24 +0000

    Include approve message inside build_ibc_token_transfer_message

--- relayer/crates/starknet-chain-components/src/impls/messages/ibc_transfer.rs
+++ relayer/crates/starknet-chain-components/src/impls/messages/ibc_transfer.rs
@@ -21,7 +21,7 @@ use hermes_test_components::chain::traits::types::memo::HasMemoType;
 use ibc::core::host::types::identifiers::PortId;
 use ibc::primitives::Timestamp;
 use starknet::accounts::Call;
-use starknet::core::types::Felt;
+use starknet::core::types::{Felt, U256};
 use starknet::macros::selector;
 
 use crate::impls::types::address::StarknetAddress;
@@ -61,6 +61,7 @@ where
     Counterparty: HasAddressType,
     Encoding: CanEncode<ViaCairo, MsgTransfer>
         + CanEncode<ViaCairo, Product![StarknetAddress]>
+        + CanEncode<ViaCairo, Product![StarknetAddress, U256]>
         + HasEncodedType<Encoded = Vec<Felt>>
         + CanDecode<ViaCairo, String>,
 {
@@ -74,76 +75,94 @@ where
         timeout_height: Option<&u64>,
         timeout_time: Option<&Timestamp>,
     ) -> Result<Vec<Chain::Message>, Chain::Error> {
+        let encoding = chain.encoding();
         let ics20_contract_address = chain.query_contract_address(PhantomData).await?;
 
-        let calldata = chain
-            .encoding()
-            .encode(&product![amount.token_address])
-            .map_err(Chain::raise_error)?;
+        let approve_message = {
+            let call_data = encoding
+                .encode(&product![ics20_contract_address, amount.quantity,])
+                .map_err(Chain::raise_error)?;
 
-        let output = chain
-            .call_contract(
-                &ics20_contract_address,
-                &selector!("ibc_token_denom"),
-                &calldata,
-                None,
-            )
-            .await?;
+            let call = Call {
+                to: amount.token_address.0,
+                selector: selector!("approve"),
+                calldata: call_data,
+            };
 
-        let prefix_denom_str = chain
-            .encoding()
-            .decode(&output)
-            .map_err(Chain::raise_error)?;
-
-        let denom = PrefixedDenom::from_str(&prefix_denom_str).map_err(Chain::raise_error)?;
-
-        let timeout_height_on_b = if let Some(timeout_height) = timeout_height {
-            Height {
-                revision_number: 0,
-                revision_height: *timeout_height,
-            }
-        } else {
-            Height {
-                revision_number: 0,
-                revision_height: 0,
-            }
+            StarknetMessage::new(call)
         };
 
-        let timeout_timestamp_on_b = if let Some(timeout_time) = timeout_time {
-            *timeout_time
-        } else {
-            Timestamp::from_nanoseconds(0)
+        let transfer_message = {
+            let calldata = chain
+                .encoding()
+                .encode(&product![amount.token_address])
+                .map_err(Chain::raise_error)?;
+
+            let prefix_denom_str = {
+                let output = chain
+                    .call_contract(
+                        &ics20_contract_address,
+                        &selector!("ibc_token_denom"),
+                        &calldata,
+                        None,
+                    )
+                    .await?;
+
+                encoding.decode(&output).map_err(Chain::raise_error)?
+            };
+
+            let denom = PrefixedDenom::from_str(&prefix_denom_str).map_err(Chain::raise_error)?;
+
+            let timeout_height_on_b = if let Some(timeout_height) = timeout_height {
+                Height {
+                    revision_number: 0,
+                    revision_height: *timeout_height,
+                }
+            } else {
+                Height {
+                    revision_number: 0,
+                    revision_height: 0,
+                }
+            };
+
+            let timeout_timestamp_on_b = if let Some(timeout_time) = timeout_time {
+                *timeout_time
+            } else {
+                Timestamp::from_nanoseconds(0)
+            };
+
+            let memo = if let Some(memo) = memo {
+                memo.clone()
+            } else {
+                String::new()
+            };
+
+            let ics20_transfer_message = MsgTransfer {
+                port_id_on_a: port_id.clone(),
+                chan_id_on_a: channel_id.clone(),
+                denom,
+                amount: amount.quantity,
+                receiver: recipient_address.to_string(),
+                memo,
+                timeout_height_on_b,
+                timeout_timestamp_on_b,
+            };
+
+            let call_data = chain
+                .encoding()
+                .encode(&ics20_transfer_message)
+                .map_err(Chain::raise_error)?;
+
+            let call = Call {
+                to: ics20_contract_address.0,
+                selector: selector!("send_transfer"),
+                calldata: call_data,
+            };
+
+            StarknetMessage::new(call)
         };
 
-        let memo = if let Some(memo) = memo {
-            memo.clone()
-        } else {
-            String::new()
-        };
-
-        let ics20_transfer_message = MsgTransfer {
-            port_id_on_a: port_id.clone(),
-            chan_id_on_a: channel_id.clone(),
-            denom,
-            amount: amount.quantity,
-            receiver: recipient_address.to_string(),
-            memo,
-            timeout_height_on_b,
-            timeout_timestamp_on_b,
-        };
-
-        let call_data = chain
-            .encoding()
-            .encode(&ics20_transfer_message)
-            .map_err(Chain::raise_error)?;
-
-        let call = Call {
-            to: ics20_contract_address.0,
-            selector: selector!("send_transfer"),
-            calldata: call_data,
-        };
-
-        let messages = vec![StarknetMessage::new(call)];
+        let messages = vec![approve_message, transfer_message];
 
         Ok(messages)
     }
