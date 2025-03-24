428aab1d30a1148ea02148cca0760efaf5e108b6
Soares Chen <soares.chen@maybevoid.com>
2025-03-24 12:38:34 +0000

    Implement StarknetRelayDriver methods

--- relayer/crates/starknet-integration-tests/src/contexts/relay_driver.rs
+++ relayer/crates/starknet-integration-tests/src/contexts/relay_driver.rs
@@ -1,7 +1,63 @@
+use cgp::core::error::{ErrorRaiserComponent, ErrorTypeProviderComponent};
+use cgp::core::field::Index;
+use cgp::extra::run::CanRun;
 use cgp::prelude::*;
+use hermes_cosmos_integration_tests::contexts::relay_driver::AbortOnDrop;
+use hermes_cosmos_relayer::contexts::chain::CosmosChain;
+use hermes_error::handlers::debug::DebugError;
+use hermes_error::impls::UseHermesError;
+use hermes_error::Error;
+use hermes_relayer_components::multi::traits::birelay_at::{
+    BiRelayGetterAtComponent, BiRelayTypeProviderAtComponent,
+};
+use hermes_relayer_components::multi::traits::chain_at::ChainTypeProviderAtComponent;
+use hermes_relayer_components::multi::traits::relay_at::RelayTypeProviderAtComponent;
+use hermes_starknet_chain_context::contexts::chain::StarknetChain;
+use hermes_starknet_relayer::contexts::cosmos_to_starknet_relay::CosmosToStarknetRelay;
 use hermes_starknet_relayer::contexts::starknet_cosmos_birelay::StarknetCosmosBiRelay;
+use hermes_starknet_relayer::contexts::starknet_to_cosmos_relay::StarknetToCosmosRelay;
+use hermes_test_components::relay_driver::run::{
+    RelayerBackgroundRunner, RelayerBackgroundRunnerComponent,
+};
 
+#[cgp_context(StarknetRelayDriverComponents)]
 #[derive(HasField)]
 pub struct StarknetRelayDriver {
     pub birelay: StarknetCosmosBiRelay,
+}
+
+delegate_components! {
+    StarknetRelayDriverComponents {
+        ErrorTypeProviderComponent: UseHermesError,
+        ErrorRaiserComponent: DebugError,
+        ChainTypeProviderAtComponent<Index<0>>:
+            UseType<StarknetChain>,
+        ChainTypeProviderAtComponent<Index<1>>:
+            UseType<CosmosChain>,
+        RelayTypeProviderAtComponent<Index<0>, Index<1>>:
+            UseType<StarknetToCosmosRelay>,
+        RelayTypeProviderAtComponent<Index<1>, Index<0>>:
+            UseType<CosmosToStarknetRelay>,
+        BiRelayTypeProviderAtComponent<Index<0>, Index<1>>:
+            UseType<StarknetCosmosBiRelay>,
+        BiRelayGetterAtComponent<Index<0>, Index<1>>:
+            UseField<symbol!("birelay")>,
+    }
+}
+
+#[cgp_provider(RelayerBackgroundRunnerComponent)]
+impl RelayerBackgroundRunner<StarknetRelayDriver> for StarknetRelayDriverComponents {
+    type RunHandle<'a> = AbortOnDrop;
+
+    async fn run_relayer_in_background(
+        relay_driver: &StarknetRelayDriver,
+    ) -> Result<AbortOnDrop, Error> {
+        let birelay = relay_driver.birelay.clone();
+
+        let handle = tokio::spawn(async move {
+            let _ = birelay.run().await;
+        });
+
+        Ok(AbortOnDrop(handle.abort_handle()))
+    }
 }
