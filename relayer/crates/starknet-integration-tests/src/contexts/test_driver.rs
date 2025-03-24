dcf2d941b3ce361f39f8ee5db30063a2595e3c97
Soares Chen <soares.chen@maybevoid.com>
2025-03-24 12:33:36 +0000

    Implement methods for StarknetTestDriver

--- relayer/crates/starknet-integration-tests/src/contexts/test_driver.rs
+++ relayer/crates/starknet-integration-tests/src/contexts/test_driver.rs
@@ -1,26 +1,46 @@
+use cgp::core::error::{ErrorRaiserComponent, ErrorTypeProviderComponent};
 use cgp::core::field::Index;
 use cgp::prelude::*;
 use hermes_cosmos_integration_tests::contexts::chain_driver::CosmosChainDriver;
 use hermes_cosmos_relayer::contexts::chain::CosmosChain;
-use hermes_relayer_components::multi::traits::birelay_at::HasBiRelayTypeAt;
-use hermes_relayer_components::multi::traits::chain_at::HasChainTypeAt;
-use hermes_relayer_components::multi::traits::relay_at::HasRelayTypeAt;
+use hermes_error::handlers::debug::DebugError;
+use hermes_error::impls::UseHermesError;
+use hermes_logger::UseHermesLogger;
+use hermes_logging_components::traits::has_logger::{
+    GlobalLoggerGetterComponent, LoggerGetterComponent, LoggerTypeProviderComponent,
+};
+use hermes_relayer_components::multi::traits::birelay_at::{
+    BiRelayTypeProviderAtComponent, HasBiRelayTypeAt,
+};
+use hermes_relayer_components::multi::traits::chain_at::{
+    ChainTypeProviderAtComponent, HasChainTypeAt,
+};
+use hermes_relayer_components::multi::traits::relay_at::{
+    HasRelayTypeAt, RelayTypeProviderAtComponent,
+};
 use hermes_starknet_chain_components::types::channel_id::ChannelId;
 use hermes_starknet_chain_components::types::connection_id::ConnectionId;
 use hermes_starknet_chain_context::contexts::chain::StarknetChain;
+use hermes_starknet_relayer::contexts::cosmos_starknet_birelay::CosmosStarknetBiRelay;
 use hermes_starknet_relayer::contexts::cosmos_to_starknet_relay::CosmosToStarknetRelay;
 use hermes_starknet_relayer::contexts::starknet_cosmos_birelay::StarknetCosmosBiRelay;
 use hermes_starknet_relayer::contexts::starknet_to_cosmos_relay::StarknetToCosmosRelay;
-use hermes_test_components::driver::traits::types::chain_driver_at::HasChainDriverTypeAt;
+use hermes_test_components::driver::traits::channel_at::ChannelIdGetterAtComponent;
+use hermes_test_components::driver::traits::types::chain_driver_at::{
+    ChainDriverGetterAtComponent, ChainDriverTypeProviderAtComponent, HasChainDriverTypeAt,
+};
+use hermes_test_components::driver::traits::types::relay_driver_at::RelayDriverGetterAtComponent;
 use hermes_test_components::setup::traits::driver::HasTestDriverType;
 use hermes_test_components::setup::traits::drivers::binary_channel::{
     BinaryChannelDriverBuilder, BinaryChannelDriverBuilderComponent,
 };
+use hermes_test_components::setup::traits::port_id_at::PortIdGetterAtComponent;
 use ibc::core::host::types::identifiers::PortId;
 
 use crate::contexts::chain_driver::StarknetChainDriver;
 use crate::contexts::relay_driver::StarknetRelayDriver;
 
+#[cgp_context(StarknetTestDriverComponents)]
 #[derive(HasField)]
 pub struct StarknetTestDriver {
     pub relay_driver: StarknetRelayDriver,
@@ -32,6 +52,49 @@ pub struct StarknetTestDriver {
     pub channel_id_b: ChannelId,
     pub port_id_a: PortId,
     pub port_id_b: PortId,
+}
+
+delegate_components! {
+    StarknetTestDriverComponents {
+        ErrorTypeProviderComponent: UseHermesError,
+        ErrorRaiserComponent: DebugError,
+        [
+            LoggerTypeProviderComponent,
+            LoggerGetterComponent,
+            GlobalLoggerGetterComponent,
+        ]:
+            UseHermesLogger,
+        ChainTypeProviderAtComponent<Index<0>>:
+            UseType<StarknetChain>,
+        ChainTypeProviderAtComponent<Index<1>>:
+            UseType<CosmosChain>,
+        ChainDriverTypeProviderAtComponent<Index<0>>:
+            UseType<StarknetChainDriver>,
+        ChainDriverTypeProviderAtComponent<Index<1>>:
+            UseType<CosmosChainDriver>,
+        RelayTypeProviderAtComponent<Index<0>, Index<1>>:
+            UseType<StarknetToCosmosRelay>,
+        RelayTypeProviderAtComponent<Index<1>, Index<0>>:
+            UseType<CosmosToStarknetRelay>,
+        BiRelayTypeProviderAtComponent<Index<0>, Index<1>>:
+            UseType<StarknetCosmosBiRelay>,
+        BiRelayTypeProviderAtComponent<Index<1>, Index<0>>:
+            UseType<CosmosStarknetBiRelay>,
+        ChainDriverGetterAtComponent<Index<0>>:
+            UseField<symbol!("chain_driver_a")>,
+        ChainDriverGetterAtComponent<Index<1>>:
+            UseField<symbol!("chain_driver_b")>,
+        RelayDriverGetterAtComponent<Index<0>, Index<1>>:
+            UseField<symbol!("relay_driver")>,
+        ChannelIdGetterAtComponent<Index<0>, Index<1>>:
+            UseField<symbol!("channel_id_a")>,
+        ChannelIdGetterAtComponent<Index<1>, Index<0>>:
+            UseField<symbol!("channel_id_b")>,
+        PortIdGetterAtComponent<Index<0>, Index<1>>:
+            UseField<symbol!("port_id_a")>,
+        PortIdGetterAtComponent<Index<1>, Index<0>>:
+            UseField<symbol!("port_id_b")>,
+    }
 }
 
 #[cgp_new_provider(BinaryChannelDriverBuilderComponent)]
