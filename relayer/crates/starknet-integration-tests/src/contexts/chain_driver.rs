f28afa9624f64feb2c27724460b1dfc20ecca57b
Soares Chen <soares.chen@maybevoid.com>
2025-03-24 14:26:48 +0000

    Implement HasDenom for StarknetChainDriver

--- relayer/crates/starknet-integration-tests/src/contexts/chain_driver.rs
+++ relayer/crates/starknet-integration-tests/src/contexts/chain_driver.rs
@@ -1,16 +1,17 @@
+use core::marker::PhantomData;
 use std::collections::BTreeMap;
 use std::path::PathBuf;
 
 use cgp::core::component::UseDelegate;
 use cgp::core::error::{ErrorRaiserComponent, ErrorTypeProviderComponent};
-use cgp::core::field::WithField;
-use cgp::core::types::WithType;
 use cgp::prelude::*;
+use hermes_cosmos_test_components::chain_driver::impls::amount::GenerateRandomAmount;
 use hermes_error::impls::UseHermesError;
 use hermes_runtime::types::runtime::HermesRuntime;
 use hermes_runtime_components::traits::runtime::{
     RuntimeGetterComponent, RuntimeTypeProviderComponent,
 };
+use hermes_starknet_chain_components::impls::types::address::StarknetAddress;
 use hermes_starknet_chain_components::types::wallet::StarknetWallet;
 use hermes_starknet_chain_context::contexts::chain::StarknetChain;
 use hermes_starknet_chain_context::impls::error::HandleStarknetChainError;
@@ -19,6 +20,13 @@ use hermes_starknet_test_components::types::node_config::StarknetNodeConfig;
 use hermes_test_components::chain_driver::impls::wait::WaitChainReachHeight;
 use hermes_test_components::chain_driver::traits::chain_process::{
     ChainProcessTaker, ChainProcessTakerComponent,
+};
+use hermes_test_components::chain_driver::traits::fields::amount::RandomAmountGeneratorComponent;
+use hermes_test_components::chain_driver::traits::fields::denom::{
+    DenomGetter, DenomGetterComponent, StakingDenom, TransferDenom,
+};
+use hermes_test_components::chain_driver::traits::fields::wallet::{
+    RelayerWallet, UserWallet, WalletGetterComponent,
 };
 use hermes_test_components::chain_driver::traits::types::chain::{
     ChainGetter, ChainGetterComponent, ChainTypeComponent, HasChain, ProvideChainType,
@@ -43,11 +51,24 @@ pub struct StarknetChainDriver {
 
 delegate_components! {
     StarknetChainDriverComponents {
-        ErrorTypeProviderComponent: UseHermesError,
-        ErrorRaiserComponent: UseDelegate<HandleStarknetChainError>,
-        RuntimeTypeProviderComponent: WithType<HermesRuntime>,
-        RuntimeGetterComponent: WithField<symbol!("runtime")>,
-        ChainStartupWaiterComponent: WaitChainReachHeight<1>,
+        ErrorTypeProviderComponent:
+            UseHermesError,
+        ErrorRaiserComponent:
+            UseDelegate<HandleStarknetChainError>,
+        RuntimeTypeProviderComponent:
+            UseType<HermesRuntime>,
+        RuntimeGetterComponent:
+            UseField<symbol!("runtime")>,
+        WalletGetterComponent<RelayerWallet>:
+            UseField<symbol!("relayer_wallet")>,
+        WalletGetterComponent<UserWallet<0>>:
+            UseField<symbol!("user_wallet_a")>,
+        WalletGetterComponent<UserWallet<1>>:
+            UseField<symbol!("user_wallet_b")>,
+        ChainStartupWaiterComponent:
+            WaitChainReachHeight<1>,
+        RandomAmountGeneratorComponent:
+            GenerateRandomAmount,
     }
 }
 
@@ -60,6 +81,20 @@ impl ProvideChainType<StarknetChainDriver> for StarknetChainDriverComponents {
 impl ChainGetter<StarknetChainDriver> for StarknetChainDriverComponents {
     fn chain(driver: &StarknetChainDriver) -> &StarknetChain {
         &driver.chain
+    }
+}
+
+#[cgp_provider(DenomGetterComponent<TransferDenom>)]
+impl DenomGetter<StarknetChainDriver, TransferDenom> for StarknetChainDriverComponents {
+    fn denom(driver: &StarknetChainDriver, _index: PhantomData<TransferDenom>) -> &StarknetAddress {
+        &driver.genesis_config.transfer_denom
+    }
+}
+
+#[cgp_provider(DenomGetterComponent<StakingDenom>)]
+impl DenomGetter<StarknetChainDriver, StakingDenom> for StarknetChainDriverComponents {
+    fn denom(driver: &StarknetChainDriver, _index: PhantomData<StakingDenom>) -> &StarknetAddress {
+        &driver.genesis_config.staking_denom
     }
 }
 
