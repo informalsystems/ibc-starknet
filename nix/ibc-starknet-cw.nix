40295fc02946b12aca57c51f01ed8cf5cb4cfad3
Soares Chen <soares.chen@maybevoid.com>
2025-03-26 12:39:06 +0000

    Use back main branches

--- nix/ibc-starknet-cw.nix
+++ nix/ibc-starknet-cw.nix
@@ -10,8 +10,8 @@ let
     cargoLock = {
       lockFile = ./../light-client/Cargo.lock;
       outputHashes = {
-        "cgp-0.4.0" = "sha256-hbRKZBEtNx3fwU35OaArxJDPbrST7OktimjUb02NZVA=";
-        "hermes-cosmos-encoding-components-0.1.0" = "sha256-FNVcPfl8FX+Apz31YRZYenGxM1y6C27H/sfw4///294=";
+        "cgp-0.4.0" = "sha256-I3LGUI8pJuKnDEEM6B7UCHdkQJjINdJB7P98Mkh5Ek8=";
+        "hermes-cosmos-encoding-components-0.1.0" = "sha256-9DTeR2zsGo7Ev4k6fJFNTvAA1fYqJ7sPHNT/naRVe9g=";
         "ibc-client-cw-0.56.0" = "sha256-DA3AB8ejUrx4ksBtN/vaOznjpKE0+0F6vGA7JmWyHWA=";
         "ibc-0.56.0" = "sha256-7DPIqu/zs0szjmtJTfXI2eQ0HEkRyvGjArcMZsFWMT4=";
       };
