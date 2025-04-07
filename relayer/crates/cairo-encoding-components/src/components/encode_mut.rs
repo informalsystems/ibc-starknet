9790f08ff9c0f4c49bef4498f94801cb5143440b
Ranadeep Biswas <mail@rnbguy.at>
2025-04-07 15:39:29 +0200

    cairo encode signed integers

--- relayer/crates/cairo-encoding-components/src/components/encode_mut.rs
+++ relayer/crates/cairo-encoding-components/src/components/encode_mut.rs
@@ -6,7 +6,9 @@ mod preset {
     use crate::impls::encode_mut::array::EncodeArray;
     use crate::impls::encode_mut::bool::EncodeBool;
     use crate::impls::encode_mut::felt::EncodeFelt;
+    use crate::impls::encode_mut::from_i128::EncodeFromI128;
     use crate::impls::encode_mut::from_u128::EncodeFromU128;
+    use crate::impls::encode_mut::i128::EncodeI128;
     use crate::impls::encode_mut::string::EncodeUtf8String;
     use crate::impls::encode_mut::u128::EncodeU128;
     use crate::impls::encode_mut::u256::EncodeU256;
@@ -18,6 +20,7 @@ mod preset {
         CairoEncodeMutComponents {
             (ViaCairo, Felt): EncodeFelt,
             (ViaCairo, u128): EncodeU128,
+            (ViaCairo, i128): EncodeI128,
             (ViaCairo, U256): EncodeU256,
             (ViaCairo, Vec<u8>): EncodeList,
             (ViaCairo, Vec<Felt>): EncodeList,
@@ -26,12 +29,18 @@ mod preset {
             (ViaCairo, u64): EncodeFromU128,
             (ViaCairo, u32): EncodeFromU128,
             (ViaCairo, usize): EncodeFromU128,
+            (ViaCairo, i8): EncodeFromI128,
+            (ViaCairo, i64): EncodeFromI128,
+            (ViaCairo, i32): EncodeFromI128,
+            (ViaCairo, isize): EncodeFromI128,
             (ViaCairo, String): EncodeUtf8String,
             (ViaCairo, ()): EncodeNothing,
             (ViaCairo, Nil): EncodeNothing,
             (ViaCairo, Vec<String>): EncodeList,
             // TODO(rano): use <const N: usize>
             (ViaCairo, [String; 2]): EncodeArray,
+            // For Tendermint Validator AccountId
+            (ViaCairo, [u8; 20]): EncodeArray,
             (ViaCairo, [u32; 8]): EncodeArray,
         }
     }
