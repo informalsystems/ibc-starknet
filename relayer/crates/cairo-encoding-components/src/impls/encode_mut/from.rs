b0f3dfbf69d2bbe47efcea1e9e0c4fe2fe5ccdc6
Soares Chen <soares.chen@gmail.com>
2025-04-10 22:21:17 +0200

    Use `#[derive(HasFields)]` and datatype-generic encoders to simplify Cairo encoding implementations (#382)
    * Update cgp and derive HasFields for PrefixedDenom
    
    * Use EncodeFields to encode TracePrefix and PrefixedDenom
    
    * Implement EncodeEnumFields to encode enums
    
    * Remove ad hoc encoder implementations for PrefixedDenom
    
    * Fix formatting
    
    * Update cgp
    
    * Update light client
    
    * Fix formatting
    
    * Use EncodeFields for connection and channel handshake messages
    
    * Use EncodeFields to implement encoding for other local types
    
    * Refactor HasAddressType and others
    
    * Use EncodeDisplay to encode string-like types
    
    * Simplify ad hoc encoding of ChannelEnd
    
    * Fix clippy
    
    * Fix channel counterparty encoding
    
    * Check for exactly one connection id in connection_hops
    
    * Fix non-standard encoding of counterparty channel id
    
    * Fix clippy
    
    * Simplify definition of SumEncoders
    
    * Update hermes-sdk
    
    * Update cgp
    
    * Update hermes-sdk
    
    * Use height-based auto relaying to clear timeout packets
    
    * Use back main branch

---
+++ relayer/crates/cairo-encoding-components/src/impls/encode_mut/from.rs
@@ -0,0 +1,48 @@
+use core::marker::PhantomData;
+
+use cgp::prelude::*;
+use hermes_encoding_components::traits::decode_mut::{MutDecoder, MutDecoderComponent};
+use hermes_encoding_components::traits::encode_mut::{MutEncoder, MutEncoderComponent};
+use hermes_encoding_components::traits::types::decode_buffer::HasDecodeBufferType;
+use hermes_encoding_components::traits::types::encode_buffer::HasEncodeBufferType;
+
+pub struct EncodeFrom<Intermediate, InEncoder>(pub PhantomData<(Intermediate, InEncoder)>);
+
+#[cgp_provider(MutEncoderComponent)]
+impl<Encoding, Strategy, Value, Intermediate, InEncoder> MutEncoder<Encoding, Strategy, Value>
+    for EncodeFrom<Intermediate, InEncoder>
+where
+    Encoding: HasEncodeBufferType + HasAsyncErrorType,
+    Intermediate: for<'a> From<&'a Value>,
+    InEncoder: MutEncoder<Encoding, Strategy, Intermediate>,
+{
+    fn encode_mut(
+        encoding: &Encoding,
+        value: &Value,
+        buffer: &mut Encoding::EncodeBuffer,
+    ) -> Result<(), Encoding::Error> {
+        let intermediate = value.into();
+        InEncoder::encode_mut(encoding, &intermediate, buffer)?;
+
+        Ok(())
+    }
+}
+
+#[cgp_provider(MutDecoderComponent)]
+impl<Encoding, Strategy, Value, Intermediate, InEncoder> MutDecoder<Encoding, Strategy, Value>
+    for EncodeFrom<Intermediate, InEncoder>
+where
+    Encoding: HasDecodeBufferType + CanRaiseAsyncError<Intermediate::Error>,
+    InEncoder: MutDecoder<Encoding, Strategy, Intermediate>,
+    Intermediate: TryInto<Value>,
+{
+    fn decode_mut<'a>(
+        encoding: &Encoding,
+        buffer: &mut Encoding::DecodeBuffer<'a>,
+    ) -> Result<Value, Encoding::Error> {
+        let intermediate = InEncoder::decode_mut(encoding, buffer)?;
+        let value = intermediate.try_into().map_err(Encoding::raise_error)?;
+
+        Ok(value)
+    }
+}
