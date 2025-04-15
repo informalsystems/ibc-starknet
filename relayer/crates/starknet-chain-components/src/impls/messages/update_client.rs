9bb2cf94e5aae13cf87c93c6582f371409b1e5b7
Ranadeep Biswas <mail@rnbguy.at>
2025-04-15 14:32:15 +0200

    add comment

--- relayer/crates/starknet-chain-components/src/impls/messages/update_client.rs
+++ relayer/crates/starknet-chain-components/src/impls/messages/update_client.rs
@@ -56,6 +56,13 @@ where
 
             let contract_address = chain.query_contract_address(PhantomData).await?;
 
+            // We are not passing the Cairo serialization of the Client Header here.
+            // As it has a lot of hash fields as `Vec<u8>`. In the Cairo serialization,
+            // they are be encoded as `Array<felt252>` wasting a lot of encoding data space.
+            //
+            // So, we encode the Header as Protobuf bytes and then encode those bytes as
+            // Cairo `ByteArray` which has more succinct `Vec<u8>` representation.
+
             let protobuf_bytes = Protobuf::<RawHeader>::encode_vec(header.clone());
 
             let protobuf_byte_array: ByteArray = protobuf_bytes.into();
