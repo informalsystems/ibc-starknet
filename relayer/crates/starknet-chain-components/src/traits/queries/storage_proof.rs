9da51c34e58cd4baa273da8ce3094698e8fc11bb
Soares Chen <soares.chen@maybevoid.com>
2025-04-17 09:58:45 +0000

    Add CanQueryStorageProof trait

---
+++ relayer/crates/starknet-chain-components/src/traits/queries/storage_proof.rs
@@ -0,0 +1,17 @@
+use cgp::prelude::*;
+use hermes_chain_components::traits::types::height::HasHeightType;
+use hermes_chain_type_components::traits::types::address::HasAddressType;
+
+use crate::traits::types::storage_proof::{HasStorageKeyType, HasStorageProofType};
+
+#[async_trait]
+pub trait CanQueryStorageProof:
+    HasHeightType + HasAddressType + HasStorageKeyType + HasStorageProofType + HasAsyncErrorType
+{
+    async fn query_storage_proof(
+        &self,
+        height: &Self::Height,
+        contract_address: &Self::Address,
+        storage_keys: &[Self::StorageKey],
+    ) -> Result<Self::StorageProof, Self::Error>;
+}
