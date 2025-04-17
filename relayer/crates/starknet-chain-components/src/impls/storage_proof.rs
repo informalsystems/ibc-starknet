4d7356b0afa7ff4add8b6d43108c759b0678a0ee
Soares Chen <soares.chen@maybevoid.com>
2025-04-17 13:10:50 +0000

    Add verify_merkle_node_map helper

--- relayer/crates/starknet-chain-components/src/impls/storage_proof.rs
+++ relayer/crates/starknet-chain-components/src/impls/storage_proof.rs
@@ -1,6 +1,7 @@
 use cgp::prelude::{CanRaiseError, HasErrorType};
+use indexmap::IndexMap;
 use starknet::core::crypto::pedersen_hash;
-use starknet::core::types::{MerkleNode, StorageProof};
+use starknet::core::types::{Felt, MerkleNode, StorageProof};
 
 use crate::traits::types::storage_proof::HasStorageProofType;
 
@@ -13,18 +14,61 @@ where
     Chain: HasStorageProofType<StorageProof = StorageProof> + CanRaiseError<String>,
 {
     fn verify_storage_proof(proof: &StorageProof) -> Result<(), Self::Error> {
+        Chain::verify_merkle_node_map(&proof.classes_proof)?;
+        Chain::verify_merkle_node_map(&proof.contracts_proof.nodes)?;
+
         for storage_entry in proof.contracts_storage_proofs.iter() {
-            for (hash, node) in storage_entry.iter() {
-                match node {
-                    MerkleNode::BinaryNode(node) => {
-                        let expected = pedersen_hash(&node.left, &node.right);
-                        if &expected != hash {
-                            return Err(Chain::raise_error(format!(
-                                "error validating binary node {node:?}. expected hash: {expected}, got: {hash}"
-                            )));
-                        }
-                    }
-                    MerkleNode::EdgeNode(node) => {}
+            Chain::verify_merkle_node_map(storage_entry)?;
+        }
+
+        Ok(())
+    }
+}
+
+pub trait CanVerifyMerkleNodeMap: HasErrorType {
+    fn verify_merkle_node_map(node_map: &IndexMap<Felt, MerkleNode>) -> Result<(), Self::Error>;
+}
+
+impl<Chain> CanVerifyMerkleNodeMap for Chain
+where
+    Chain: CanRaiseError<String>,
+{
+    fn verify_merkle_node_map(node_map: &IndexMap<Felt, MerkleNode>) -> Result<(), Self::Error> {
+        for (hash, node) in node_map.iter() {
+            Chain::verify_merkle_node(hash, node)?;
+        }
+
+        Ok(())
+    }
+}
+
+pub trait CanVerifyMerkleNode: HasErrorType {
+    fn verify_merkle_node(node_hash: &Felt, node: &MerkleNode) -> Result<(), Self::Error>;
+}
+
+impl<Chain> CanVerifyMerkleNode for Chain
+where
+    Chain: CanRaiseError<String>,
+{
+    fn verify_merkle_node(node_hash: &Felt, node: &MerkleNode) -> Result<(), Self::Error> {
+        match node {
+            MerkleNode::BinaryNode(node) => {
+                let expected = pedersen_hash(&node.left, &node.right);
+
+                if &expected != node_hash {
+                    return Err(Chain::raise_error(format!(
+                        "error validating binary node {node:?}. expected hash: {expected}, got: {node_hash}"
+                    )));
+                }
+            }
+
+            MerkleNode::EdgeNode(node) => {
+                let expected = pedersen_hash(&node.child, &node.path) + node.length;
+
+                if &expected != node_hash {
+                    return Err(Chain::raise_error(format!(
+                        "error validating edge node {node:?}. expected hash: {expected}, got: {node_hash}"
+                    )));
                 }
             }
         }
