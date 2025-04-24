b00dc7a542dbc0a167a39823d3e0290c5fe0e655
Soares Chen <soares.chen@maybevoid.com>
2025-04-24 10:31:28 +0000

    Latest Madara has fixed get_transaction_receipt

--- relayer/crates/starknet-madara-tests/src/impls/queries/tx_response.rs
+++ relayer/crates/starknet-madara-tests/src/impls/queries/tx_response.rs
@@ -9,21 +9,19 @@ use hermes_relayer_components::transaction::traits::types::tx_response::HasTxRes
 use hermes_runtime_components::traits::runtime::HasRuntime;
 use hermes_runtime_components::traits::sleep::CanSleep;
 use hermes_starknet_chain_components::traits::client::HasStarknetClient;
-use hermes_starknet_chain_components::traits::json_rpc::CanSendJsonRpcRequest;
-use serde::{Deserialize, Serialize};
-use starknet_v13::core::types::requests::TraceTransactionRequest;
-use starknet_v13::core::types::{Felt, StarknetError, TransactionTrace};
+use starknet_v13::core::types::{Felt, StarknetError};
 use starknet_v13::providers::{Provider, ProviderError};
 
 use crate::types::TxResponse;
 
-#[cgp_new_provider(TxResponseQuerierComponent)]
+pub struct QueryTransactionReceipt;
+
+#[cgp_provider(TxResponseQuerierComponent)]
 impl<Chain> TxResponseQuerier<Chain> for QueryTransactionReceipt
 where
     Chain: HasTxHashType<TxHash = Felt>
         + HasTxResponseType<TxResponse = TxResponse>
         + HasStarknetClient<Client: Provider>
-        + CanTraceTransaction
         + HasRuntime<Runtime: CanSleep>
         + CanRaiseAsyncError<ProviderError>,
 {
@@ -37,7 +35,10 @@ where
 
         match result {
             Ok(receipt) => {
-                let trace = chain.trace_transaction(*tx_hash).await?;
+                let trace = provider
+                    .trace_transaction(tx_hash)
+                    .await
+                    .map_err(Chain::raise_error)?;
 
                 // Wait for a second for the starknet-devnet chain to progress.
                 // We may not need this when we transition to a production chain.
@@ -49,38 +50,4 @@ where
             Err(e) => Err(Chain::raise_error(e)),
         }
     }
-}
-
-// Madara returns a non-compliant JSON RPC response for the starknet_traceTransaction call.
-// Hence we need a workaround to parse the response manually here.
-// We may need further abstraction over the underlying JSON RPC client if there are other
-// non-compliant APIs out there.
-#[async_trait]
-pub trait CanTraceTransaction: HasAsyncErrorType {
-    async fn trace_transaction(&self, tx_hash: Felt) -> Result<TransactionTrace, Self::Error>;
-}
-
-impl<Chain> CanTraceTransaction for Chain
-where
-    Chain: for<'a> CanSendJsonRpcRequest<TraceTransactionRequest, TraceTransactionResponse>,
-{
-    async fn trace_transaction(
-        &self,
-        transaction_hash: Felt,
-    ) -> Result<TransactionTrace, Self::Error> {
-        let params = TraceTransactionRequest { transaction_hash };
-
-        let rpc_response = self
-            .send_json_rpc_request("starknet_traceTransaction", &params)
-            .await?;
-
-        let trace = rpc_response.trace_root;
-
-        Ok(trace)
-    }
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TraceTransactionResponse {
-    pub trace_root: TransactionTrace,
 }
