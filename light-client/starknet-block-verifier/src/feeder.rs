96429901c8f69637f9171ceeeaab4657c6d305fb
Rano | Ranadeep <ranadeep@informal.systems>
2025-05-22 23:18:48 +0200

    feat: verify starknet block from centralized sequencer (#428)
    * starknet-block-verifier crate
    
    * add crate to workspace members
    
    * use starknet 0.13.5 blocks
    
    * use ureq over reqwest
    
    * avoid using async
    
    * format toml file
    
    * use nextest on CI
    
    * update Cargo lockfile
    
    * use curl to download madara bin
    
    * rm dbg statement
    
    * add starknet-block-verifier dependency to relayer workspace
    
    * add starknet-block-verifer dep to madara test
    
    * add feeder gateware signature verifier
    
    * update Cargo lockfile
    
    * fix gateway port
    
    * try to fix madara signature test
    
    * madara feeder api requires block heights
    
    * refactor and comment
    
    * add dev deps
    
    * refactor starknet block verifier tests
    
    * update Cargo lockfile
    
    * fix typo in tests
    
    * make const available by default
    
    * add comments
    
    * rm comment
    
    * apply review suggestions
    
    * make ureq optional

---
+++ light-client/starknet-block-verifier/src/feeder.rs
@@ -0,0 +1,53 @@
+use serde::de::DeserializeOwned;
+use starknet_crypto::Felt;
+
+use crate::{Block, Signature, MAINNET_FEEDER_URL, SEPOLIA_FEEDER_URL};
+
+// https://community.starknet.io/t/feeder-gateway-deprecation/100233#full-list-of-endpoints-3
+const GET_BLOCK_PATH: &str = "get_block";
+const GET_PUBLIC_KEY: &str = "get_public_key";
+const GET_SIGNATURE: &str = "get_signature";
+
+pub struct Endpoint(pub String);
+
+impl Endpoint {
+    pub fn new(endpoint: &str) -> Self {
+        Self(endpoint.to_string())
+    }
+
+    pub fn sepolia() -> Self {
+        Self(SEPOLIA_FEEDER_URL.to_string())
+    }
+
+    pub fn mainnet() -> Self {
+        Self(MAINNET_FEEDER_URL.to_string())
+    }
+
+    pub fn get<T: DeserializeOwned>(
+        &self,
+        path: &'static str,
+        block_number: Option<u64>,
+    ) -> Result<T, ureq::Error> {
+        let mut req = ureq::get(&format!("{}/feeder_gateway/{}", self.0, path));
+
+        if let Some(block_number) = block_number {
+            req = req.query("blockNumber", block_number.to_string());
+        }
+
+        let result = req.call()?.body_mut().read_json()?;
+
+        Ok(result)
+    }
+
+    pub fn get_block_header(&self, block_number: Option<u64>) -> Result<Block, ureq::Error> {
+        self.get(GET_BLOCK_PATH, block_number)
+    }
+
+    pub fn get_public_key(&self, block_number: Option<u64>) -> Result<Felt, ureq::Error> {
+        self.get(GET_PUBLIC_KEY, block_number)
+    }
+
+    pub fn get_signature(&self, block_number: Option<u64>) -> Result<Signature, ureq::Error> {
+        self.get(GET_SIGNATURE, block_number)
+    }
+}
