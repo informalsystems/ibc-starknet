019fc06a8e242e23324750f901a827d17194d3e6
Ranadeep Biswas <mail@rnbguy.at>
2025-06-30 11:29:58 +0200

    use json fixtures

--- light-client/starknet-storage-verifier/src/error.rs
+++ light-client/starknet-storage-verifier/src/error.rs
@@ -101,3 +101,37 @@ impl From<TryFromIntError> for StorageError {
         Self::TryFromIntError
     }
 }
+
+impl core::error::Error for StorageError {
+    fn source(&self) -> Option<&(dyn core::error::Error + 'static)> {
+        None
+    }
+}
+
+impl core::fmt::Display for StorageError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        match self {
+            Self::ChildNodeWithZeroValue => write!(f, "Child node contains zero value, which is invalid as the whole node must be omitted"),
+            Self::ChildNodeMismatchValue => write!(f, "Child node contains a value that does not match the expected value"),
+            Self::CommitmentPathExceedUpper => write!(f, "Commitment path exceeds felt upper bound"),
+            Self::Generic(msg) => write!(f, "{msg}"),
+            Self::InvalidEdgeNode => write!(f, "Invalid edge node"),
+            Self::InvalidProof => write!(f, "Malformed proof that exceeds maximum depth of 251"),
+            Self::MismatchBinaryHash => write!(f, "Error validating binary node. Expected hash does not match the computed hash"),
+            Self::MismatchEdgeHash => write!(f, "Error validating edge node. Expected hash does not match the computed hash"),
+            Self::MismatchPathSize => write!(f, "Sliced paths should have the same size"),
+            Self::MissingContractLeafNode => write!(f, "Contract leaf node not found"),
+            Self::MissingContractStorageProof => write!(f, "Contract storage proof not found"),
+            Self::MissingContractStorageRoot => write!(f, "Contract storage root not found"),
+            Self::MissingStorageRoot => write!(f, "Storage root not found"),
+            Self::MissingContractHash => write!(f, "Contract hash not found"),
+            Self::MissingParentNode => write!(f, "Failed to find parent node for child node"),
+            Self::MissingRootProofNode => write!(f, "Failed to find root proof node"),
+            Self::MissingProofNode => write!(f, "Failed to find proof node"),
+            Self::MissingValue => write!(f, "Expected value to be present, but non-membership proof is found"),
+            Self::NonZeroBit => write!(f, "Node path bit at index should be zero"),
+            Self::ZeroEdgeNode => write!(f, "Invalid edge node with zero node length"),
+            Self::TryFromIntError => write!(f, "Failed to convert integer type"),
+        }
+    }
+}
--- light-client/starknet-storage-verifier/tests/storage_proof.rs
+++ light-client/starknet-storage-verifier/tests/storage_proof.rs
@@ -9,112 +9,14 @@ use starknet_storage_verifier::verifier::{
     verify_starknet_contract_proof, verify_starknet_global_contract_root,
     verify_starknet_merkle_proof, verify_starknet_storage_proof,
 };
+use starknet_storage_verifier::StorageError;
 
 #[test]
-fn test_verify_starknet_storage_proof() {
-    let global_roots = GlobalRoots {
-        contracts_tree_root: felt!(
-            "0x368991d64cd97e90a9da1fd9f3d676875d5d29b7136a6ecf77ddc35704f4c27"
-        ),
-        classes_tree_root: felt!(
-            "0x50c234027c744bb8baf77f2229f0433804e8fb9ceb30ad21fde94698832edd1"
-        ),
-        block_hash: felt!("0x4a538abd420c852e990133d802ecaaa4b4bc2c76371b595a3e837135c63fbb0"),
-    };
-    let mut nodes = IndexMap::new();
-    nodes.insert(
-        felt!("0x2ed240ee95949cd56bb6d1feb5974abd931b844fd090f6738c1166ab129f256"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: Felt::ZERO,
-            length: 1,
-            child: felt!("0x69caa1781f8cd2bd601d17d668e4268a609430ada713cbfd97c4f1a7b79a37e"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x3fb08aa8d7ae250618a5eded665e72102624aca7988f30c1abf93478c918624"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: felt!("0x17d0ad6ddbc7e97208e2639fc5bbf9856b4ede9a66a5995aec87b0d45837c"),
-            length: 244,
-            child: felt!("0x5cea18c46bb31c421e40f94efd51a83ec4882ae72473bb41edd2652137b938e"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x368991d64cd97e90a9da1fd9f3d676875d5d29b7136a6ecf77ddc35704f4c27"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x136287afa5c7e9d96deba14d1080672eef35240cc4940076d378e84ef6b7c26"),
-            right: felt!("0xbe9a5b8558021942adf479733d2e345c5147b18d6c0aee40e5ee9fb85bc32"),
-        }),
-    );
-    nodes.insert(
-        felt!("0xe1081c844581366e4e04cd3523b382a40865746a8cd035e41c9806327852a0"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x5dc3360ed7d92cfc8acfc3d5eb35ce9402a2e6e0754ed82487f78630ee63e1d"),
-            right: felt!("0x54cb87a02896d50c9822961eb21c41326a3730e03beebb4273e9f53ecb1aa10"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x3d72d82b1e3e3eeda9fe96221dc8a2131f22e243382d0f16883568f927589ef"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0xe1081c844581366e4e04cd3523b382a40865746a8cd035e41c9806327852a0"),
-            right: felt!("0x7f5c3d3f9a27468b74d8d9352d6104a12e51257bb9bd2b775bcef738eba3d88"),
-        }),
-    );
-    nodes.insert(
-        felt!("0xbe9a5b8558021942adf479733d2e345c5147b18d6c0aee40e5ee9fb85bc32"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x3d72d82b1e3e3eeda9fe96221dc8a2131f22e243382d0f16883568f927589ef"),
-            right: felt!("0x50b7568824156bac7669bda45c4db7c579f6b799bd3fd3142332ecd083cceb6"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x5dc3360ed7d92cfc8acfc3d5eb35ce9402a2e6e0754ed82487f78630ee63e1d"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x2ed240ee95949cd56bb6d1feb5974abd931b844fd090f6738c1166ab129f256"),
-            right: felt!("0x4e50fb911906759c79313749ede30a1888a29e85e297f128cd9df6ee3a4cabb"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x69caa1781f8cd2bd601d17d668e4268a609430ada713cbfd97c4f1a7b79a37e"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x3fb08aa8d7ae250618a5eded665e72102624aca7988f30c1abf93478c918624"),
-            right: felt!("0x3781d4c7ddb1f323275a2640bb37924c979225308808987b3b16b7dd7893f1a"),
-        }),
-    );
+fn test_verify_starknet_storage_proof() -> Result<(), Box<dyn core::error::Error>> {
+    let storage_proof = serde_json::from_reader(std::fs::File::open(
+        "tests/fixtures/storage_proof_success.json",
+    )?)?;
 
-    let contracts_proof = ContractsProof {
-        contract_leaves_data: vec![ContractLeafData {
-            class_hash: felt!("0x45ba727abaff9ae3a4311d7a30196e09d1f30aeeb3a8e157277793740d20f61"),
-            nonce: Felt::ZERO,
-            storage_root: Some(felt!(
-                "0x4592da9795f9fd7a042eb0cb0d4dae7b6894bd90ccb3e6ff360185db24301f7"
-            )),
-        }],
-        nodes,
-    };
-
-    let mut contracts_storage_proof = IndexMap::new();
-    contracts_storage_proof.insert(
-        felt!("0x4592da9795f9fd7a042eb0cb0d4dae7b6894bd90ccb3e6ff360185db24301f7"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x4a73c78eb32a0614903b17bbc06bd8a7a28ca707981aa36bbe6cd18c2cbb92b"),
-            right: felt!("0x4352b8ed6f017cb4cb7084f64af3f1858db935ed9bf734ba5078e774f8a9097"),
-        }),
-    );
-    contracts_storage_proof.insert(
-        felt!("0x4a73c78eb32a0614903b17bbc06bd8a7a28ca707981aa36bbe6cd18c2cbb92b"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: felt!("0x1"),
-            length: 250,
-            child: felt!("0x9911"),
-        }),
-    );
-
-    let storage_proof = StorageProof {
-        classes_proof: Default::default(),
-        global_roots,
-        contracts_proof,
-        contracts_storage_proofs: vec![contracts_storage_proof],
-    };
     let key: Felt = felt!("0x0001");
     let value: Felt = felt!("0x9911");
     let contract_address: Felt =
@@ -122,23 +24,21 @@ fn test_verify_starknet_storage_proof() {
 
     let state_root = felt!("0x2bba45af2d71e57b1f82f1668bc53184762e6212c22e69f9949e3a607022fd2");
 
-    let verification_result = validate_storage_proof(&storage_proof)
-        .and_then(|_| verify_starknet_global_contract_root(&storage_proof, state_root))
-        .and_then(|global_contract_trie_root| {
-            verify_starknet_contract_proof(
-                &storage_proof,
-                global_contract_trie_root,
-                contract_address,
-            )
-        })
-        .and_then(|contract_root| {
-            verify_starknet_storage_proof(&storage_proof, contract_root, key, value)
-        });
+    validate_storage_proof(&storage_proof)?;
+    let global_contract_trie_root =
+        verify_starknet_global_contract_root(&storage_proof, state_root)?;
+    let contract_root = verify_starknet_contract_proof(
+        &storage_proof,
+        global_contract_trie_root,
+        contract_address,
+    )?;
 
-    assert!(
-        verification_result.is_ok(),
-        "Storage proof verification failed"
-    );
+    Ok(verify_starknet_storage_proof(
+        &storage_proof,
+        contract_root,
+        key,
+        value,
+    )?)
 }
 
 #[test]
@@ -225,111 +125,11 @@ fn test_verify_starknet_merkle_proof() {
 }
 
 #[test]
-fn test_verify_starknet_storage_proof_failure() {
-    let global_roots = GlobalRoots {
-        contracts_tree_root: felt!(
-            "0x368991d64cd97e90a9da1fd9f3d676875d5d29b7136a6ecf77ddc35704f4c27"
-        ),
-        classes_tree_root: felt!(
-            "0x50c234027c744bb8baf77f2229f0433804e8fb9ceb30ad21fde94698832edd1"
-        ),
-        block_hash: felt!("0x4a538abd420c852e990133d802ecaaa4b4bc2c76371b595a3e837135c63fbb0"),
-    };
-    let mut nodes = IndexMap::new();
-    nodes.insert(
-        felt!("0x2ed240ee95949cd56bb6d1feb5974abd931b844fd090f6738c1166ab129f256"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: Felt::ZERO,
-            length: 1,
-            child: felt!("0x69caa1781f8cd2bd601d17d668e4268a609430ada713cbfd97c4f1a7b79a37e"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x3fb08aa8d7ae250618a5eded665e72102624aca7988f30c1abf93478c918624"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: felt!("0x17d0ad6ddbc7e97208e2639fc5bbf9856b4ede9a66a5995aec87b0d45837c"),
-            length: 244,
-            child: felt!("0x5cea18c46bb31c421e40f94efd51a83ec4882ae72473bb41edd2652137b938e"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x368991d64cd97e90a9da1fd9f3d676875d5d29b7136a6ecf77ddc35704f4c27"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x136287afa5c7e9d96deba14d1080672eef35240cc4940076d378e84ef6b7c26"),
-            right: felt!("0xbe9a5b8558021942adf479733d2e345c5147b18d6c0aee40e5ee9fb85bc32"),
-        }),
-    );
-    nodes.insert(
-        felt!("0xe1081c844581366e4e04cd3523b382a40865746a8cd035e41c9806327852a0"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x5dc3360ed7d92cfc8acfc3d5eb35ce9402a2e6e0754ed82487f78630ee63e1d"),
-            right: felt!("0x54cb87a02896d50c9822961eb21c41326a3730e03beebb4273e9f53ecb1aa10"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x3d72d82b1e3e3eeda9fe96221dc8a2131f22e243382d0f16883568f927589ef"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0xe1081c844581366e4e04cd3523b382a40865746a8cd035e41c9806327852a0"),
-            right: felt!("0x7f5c3d3f9a27468b74d8d9352d6104a12e51257bb9bd2b775bcef738eba3d88"),
-        }),
-    );
-    nodes.insert(
-        felt!("0xbe9a5b8558021942adf479733d2e345c5147b18d6c0aee40e5ee9fb85bc32"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x3d72d82b1e3e3eeda9fe96221dc8a2131f22e243382d0f16883568f927589ef"),
-            right: felt!("0x50b7568824156bac7669bda45c4db7c579f6b799bd3fd3142332ecd083cceb6"),
-        }),
-    );
-    nodes.insert(
-        felt!("0x5dc3360ed7d92cfc8acfc3d5eb35ce9402a2e6e0754ed82487f78630ee63e1d"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x2ed240ee95949cd56bb6d1feb5974abd931b844fd090f6738c1166ab129f256"),
-            right: felt!("0x4e50fb911906759c79313749ede30a1888a29e85e297f128cd9df6ee3a4cabb"),
-        }),
-    );
-    // left value modified to invalid value
-    nodes.insert(
-        felt!("0x69caa1781f8cd2bd601d17d668e4268a609430ada713cbfd97c4f1a7b79a37e"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0xaaaaaaaad7ae250618a5eded665e72102624aca7988f30c1abf93478c918624"),
-            right: felt!("0x3781d4c7ddb1f323275a2640bb37924c979225308808987b3b16b7dd7893f1a"),
-        }),
-    );
+fn test_verify_starknet_storage_proof_failure() -> Result<(), Box<dyn core::error::Error>> {
+    let storage_proof = serde_json::from_reader(std::fs::File::open(
+        "tests/fixtures/storage_proof_failure.json",
+    )?)?;
 
-    let contracts_proof = ContractsProof {
-        contract_leaves_data: vec![ContractLeafData {
-            class_hash: felt!("0x45ba727abaff9ae3a4311d7a30196e09d1f30aeeb3a8e157277793740d20f61"),
-            nonce: Felt::ZERO,
-            storage_root: Some(felt!(
-                "0x4592da9795f9fd7a042eb0cb0d4dae7b6894bd90ccb3e6ff360185db24301f7"
-            )),
-        }],
-        nodes,
-    };
-
-    let mut contracts_storage_proof = IndexMap::new();
-    contracts_storage_proof.insert(
-        felt!("0x4592da9795f9fd7a042eb0cb0d4dae7b6894bd90ccb3e6ff360185db24301f7"),
-        MerkleNode::BinaryNode(BinaryNode {
-            left: felt!("0x4a73c78eb32a0614903b17bbc06bd8a7a28ca707981aa36bbe6cd18c2cbb92b"),
-            right: felt!("0x4352b8ed6f017cb4cb7084f64af3f1858db935ed9bf734ba5078e774f8a9097"),
-        }),
-    );
-    contracts_storage_proof.insert(
-        felt!("0x4a73c78eb32a0614903b17bbc06bd8a7a28ca707981aa36bbe6cd18c2cbb92b"),
-        MerkleNode::EdgeNode(EdgeNode {
-            path: felt!("0x1"),
-            length: 250,
-            child: felt!("0x9911"),
-        }),
-    );
-
-    let storage_proof = StorageProof {
-        classes_proof: Default::default(),
-        global_roots,
-        contracts_proof,
-        contracts_storage_proofs: vec![contracts_storage_proof],
-    };
     let key: Felt = felt!("0x0001");
     let value: Felt = felt!("0x9911");
     let contract_address: Felt =
@@ -337,23 +137,21 @@ fn test_verify_starknet_storage_proof_failure() {
 
     let state_root = felt!("0x2bba45af2d71e57b1f82f1668bc53184762e6212c22e69f9949e3a607022fd2");
 
-    let verification_result = validate_storage_proof(&storage_proof)
-        .and_then(|_| verify_starknet_global_contract_root(&storage_proof, state_root))
-        .and_then(|global_contract_trie_root| {
-            verify_starknet_contract_proof(
-                &storage_proof,
-                global_contract_trie_root,
-                contract_address,
-            )
-        })
-        .and_then(|contract_root| {
-            verify_starknet_storage_proof(&storage_proof, contract_root, key, value)
-        });
+    validate_storage_proof(&storage_proof)?;
+    let global_contract_trie_root =
+        verify_starknet_global_contract_root(&storage_proof, state_root)?;
+    let contract_root = verify_starknet_contract_proof(
+        &storage_proof,
+        global_contract_trie_root,
+        contract_address,
+    )?;
 
-    assert!(
-        verification_result.is_err(),
-        "Storage proof verification should fail"
-    );
+    assert!(matches!(
+        verify_starknet_storage_proof(&storage_proof, contract_root, key, value),
+        Err(StorageError::MismatchBinaryHash),
+    ));
+
+    Ok(())
 }
 
 #[test]
