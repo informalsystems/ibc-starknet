ddbe40bcf68b94599be95574d71a262b00528ec9
Ranadeep Biswas <mail@rnbguy.at>
2025-07-16 00:33:27 +0200

    fix StarknetHeader proto encoding

--- light-client/ibc-client-starknet-types/src/encoding/impls/header.rs
+++ light-client/ibc-client-starknet-types/src/encoding/impls/header.rs
@@ -1,13 +1,11 @@
 use alloc::vec::Vec;
 
 use hermes_encoding_components::traits::{
-    HasEncodeBufferType, MutDecoder, MutDecoderComponent, MutEncoder, MutEncoderComponent,
+    HasDecodeBufferType, HasEncodeBufferType, MutDecoder, MutDecoderComponent, MutEncoder,
+    MutEncoderComponent,
 };
 use hermes_prelude::*;
-use hermes_protobuf_encoding_components::impls::{
-    EncodeByteField, EncodeLengthDelimitedHeader, HasProtoChunksDecodeBuffer, InvalidWireType,
-};
-use prost::bytes::BufMut;
+use hermes_protobuf_encoding_components::impls::EncodeByteField;
 
 use crate::header::StarknetHeader;
 pub struct EncodeStarknetHeader;
@@ -15,33 +13,29 @@ pub struct EncodeStarknetHeader;
 #[cgp_provider(MutEncoderComponent)]
 impl<Encoding, Strategy> MutEncoder<Encoding, Strategy, StarknetHeader> for EncodeStarknetHeader
 where
-    Encoding: HasEncodeBufferType<EncodeBuffer: BufMut> + CanRaiseAsyncError<serde_json::Error>,
-    EncodeLengthDelimitedHeader<0>: MutEncoder<Encoding, Strategy, u64>,
-    EncodeLengthDelimitedHeader<1>: MutEncoder<Encoding, Strategy, u64>,
-    EncodeLengthDelimitedHeader<2>: MutEncoder<Encoding, Strategy, u64>,
+    Encoding: HasEncodeBufferType + CanRaiseAsyncError<serde_json::Error>,
+    EncodeByteField<1>: MutEncoder<Encoding, Strategy, Vec<u8>>,
+    EncodeByteField<2>: MutEncoder<Encoding, Strategy, Vec<u8>>,
+    EncodeByteField<3>: MutEncoder<Encoding, Strategy, Vec<u8>>,
 {
     fn encode_mut(
         encoding: &Encoding,
         value: &StarknetHeader,
         buffer: &mut Encoding::EncodeBuffer,
     ) -> Result<(), Encoding::Error> {
-        EncodeByteField::<0>::encode_mut(
-            encoding,
-            &serde_json::to_vec(&value.block_header).unwrap(),
-            buffer,
-        )?;
+        let StarknetHeader {
+            block_header,
+            block_signature,
+            storage_proof,
+        } = value;
 
-        EncodeByteField::<1>::encode_mut(
-            encoding,
-            &serde_json::to_vec(&value.block_signature).unwrap(),
-            buffer,
-        )?;
+        let block_header = serde_json::to_vec(block_header).map_err(Encoding::raise_error)?;
+        let block_signature = serde_json::to_vec(block_signature).map_err(Encoding::raise_error)?;
+        let storage_proof = serde_json::to_vec(storage_proof).map_err(Encoding::raise_error)?;
 
-        EncodeByteField::<2>::encode_mut(
-            encoding,
-            &serde_json::to_vec(&value.storage_proof).unwrap(),
-            buffer,
-        )?;
+        <EncodeByteField<1>>::encode_mut(encoding, &block_header, buffer)?;
+        <EncodeByteField<2>>::encode_mut(encoding, &block_signature, buffer)?;
+        <EncodeByteField<3>>::encode_mut(encoding, &storage_proof, buffer)?;
 
         Ok(())
     }
@@ -50,25 +44,24 @@ where
 #[cgp_provider(MutDecoderComponent)]
 impl<Encoding, Strategy> MutDecoder<Encoding, Strategy, StarknetHeader> for EncodeStarknetHeader
 where
-    Encoding: HasProtoChunksDecodeBuffer
-        + CanRaiseAsyncError<serde_json::Error>
-        + CanRaiseAsyncError<InvalidWireType>
-        + for<'a> CanRaiseAsyncError<<Vec<u8> as TryFrom<&'a [u8]>>::Error>,
+    Encoding: HasDecodeBufferType + CanRaiseAsyncError<serde_json::Error>,
+    EncodeByteField<1>: MutDecoder<Encoding, Strategy, Vec<u8>>,
+    EncodeByteField<2>: MutDecoder<Encoding, Strategy, Vec<u8>>,
+    EncodeByteField<3>: MutDecoder<Encoding, Strategy, Vec<u8>>,
 {
     fn decode_mut(
         encoding: &Encoding,
         buffer: &mut Encoding::DecodeBuffer<'_>,
     ) -> Result<StarknetHeader, Encoding::Error> {
-        let block_header: Vec<u8> =
-            <EncodeByteField<0> as MutDecoder<_, Strategy, _>>::decode_mut(encoding, buffer)?;
-        let block_signature: Vec<u8> =
-            <EncodeByteField<1> as MutDecoder<_, Strategy, _>>::decode_mut(encoding, buffer)?;
-        let storage_proof: Vec<u8> =
-            <EncodeByteField<2> as MutDecoder<_, Strategy, _>>::decode_mut(encoding, buffer)?;
+        let block_header = <EncodeByteField<1>>::decode_mut(encoding, buffer)?;
+        let block_signature = <EncodeByteField<2>>::decode_mut(encoding, buffer)?;
+        let storage_proof = <EncodeByteField<3>>::decode_mut(encoding, buffer)?;
 
-        let block_header = serde_json::from_slice(&block_header).unwrap();
-        let block_signature = serde_json::from_slice(&block_signature).unwrap();
-        let storage_proof = serde_json::from_slice(&storage_proof).unwrap();
+        let block_header = serde_json::from_slice(&block_header).map_err(Encoding::raise_error)?;
+        let block_signature =
+            serde_json::from_slice(&block_signature).map_err(Encoding::raise_error)?;
+        let storage_proof =
+            serde_json::from_slice(&storage_proof).map_err(Encoding::raise_error)?;
 
         Ok(StarknetHeader {
             block_header,
