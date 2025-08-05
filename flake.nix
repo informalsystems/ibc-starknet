51eebaff43036d119b63ca00b00a62a1b8003e1f
Ranadeep Biswas <mail@rnbguy.at>
2025-08-05 16:23:25 +0200

    use nixfmt-tree

--- flake.nix
+++ flake.nix
@@ -113,7 +113,7 @@
               pkg-config
               ;
 
-            nixfmt = nixpkgs.nixfmt-rfc-style;
+            nixfmt = nixpkgs.nixfmt-tree;
           };
 
           mac-deps = nixpkgs.lib.optional nixpkgs.stdenv.isDarwin [
@@ -124,19 +124,18 @@
           shell-deps = (builtins.attrValues starknet-pkgs) ++ (builtins.attrValues tools) ++ mac-deps;
         in
         {
-          packages =
-            {
-              inherit
-                starknet-devnet
-                cairo
-                rust
-                rust-nightly
-                rust-wasm
-                ibc-starknet-cw
-                ;
-            }
-            // tools
-            // starknet-pkgs;
+          packages = {
+            inherit
+              starknet-devnet
+              cairo
+              rust
+              rust-nightly
+              rust-wasm
+              ibc-starknet-cw
+              ;
+          }
+          // tools
+          // starknet-pkgs;
 
           devShells = {
             default = nixpkgs.mkShell { buildInputs = shell-deps; };
@@ -151,7 +150,7 @@
             rust-wasm = nixpkgs.mkShell { buildInputs = [ rust-wasm ] ++ shell-deps; };
           };
 
-          formatter = tools.nixfmt;
+          formatter = tools.nixfmt-tree;
         }
       );
 }
